5218e4a92ce231f87fa5659aa63ff5a4d764f326
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2023-12-16 08:16:21 +0900

    Update main_app.py

--- ☆Webアプリ/streamlit/main_app.py
+++ ☆Webアプリ/streamlit/main_app.py
@@ -8,6 +8,8 @@ st.caption('こんにちは！Hatakeです。')
 st.subheader('説明')
 st.text('簡易なWEBアプリ「streamlit」を使って何かやろうと考えています。\n'
        'よろしくお願いします。')
+cwd = os.getcwd()
+st.text(cwd)
 #フォルダ変更　　../images/my_image.jpg
 #写真
 st.text('こちらは2022年に東京に旅行した際の写真、動画です。')
