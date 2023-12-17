7fac5f797907485fbb01ca2fbb3b91f44cb1080a
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2023-12-17 22:31:30 +0900

    Update page_1.py

--- ☆Webアプリ/streamlit/pages/page_1.py
+++ ☆Webアプリ/streamlit/pages/page_1.py
@@ -2,5 +2,6 @@ import streamlit as st
 import pandas as pd
 st.write("２０２２年１０月の東京の天気")
    
-df=pd.read_csv("/mount/src/hatake4911/☆Webアプリ/csvファイル各種/２０２２年１０月の東京の天気.csv")
+df = pd.read_csv("/mount/src/hatake4911/☆Webアプリ/csvファイル各種/２０２２年１０月の東京の天気.csv", encoding='shift_jis')
+
 st.dataframe(df)
