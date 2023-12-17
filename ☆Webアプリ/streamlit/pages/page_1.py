e42f5dc15757135f2ea286f6a6b0147fffcce63d
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2023-12-17 22:14:32 +0900

    Update page_1.py

--- ☆Webアプリ/streamlit/pages/page_1.py
+++ ☆Webアプリ/streamlit/pages/page_1.py
@@ -1,4 +1,4 @@
 import streamlit as st
 st.write("ページ1")
 st.text('売上管理')      
-df2=pd.read_csv("/mount/src/hatake4911/☆Webアプリ/csvファイル各種/4-4_sales.csv")
+df2=pd.read_csv("/mount/src/hatake4911/☆Webアプリ/csvファイル各種/２０２２年１０月の東京の天気.csv")
