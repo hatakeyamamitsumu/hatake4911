22237c53cefedb4fb8c9ea44131e79db10a85a19
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2023-12-17 19:42:43 +0900

    Update main_app.py

--- ☆Webアプリ/streamlit/main_app.py
+++ ☆Webアプリ/streamlit/main_app.py
@@ -15,11 +15,11 @@ st.text(cwd)
 #写真
 st.text('こちらは2022年に東京に旅行した際の写真、動画です。')
 #image=Image.open('skytree.png')
-st.image('/mount/src/hatake4911/☆Webアプリ/streamlit/skytree.png',use_column_width=True)
+st.image('/mount/src/hatake4911/☆Webアプリ/画像/skytree.png',use_column_width=True)
 
 # ローカルのGIFファイルのパスを指定
 # ローカルのGIFファイルのパスを指定
-video_path = '/mount/src/hatake4911/☆Webアプリ/streamlit/東京到着.gif'
+video_path = '/mount/src/hatake4911/☆Webアプリ/動画/東京到着.gif'
 # 動画を表示
 #st.video(video_path)
 st.image(video_path)
