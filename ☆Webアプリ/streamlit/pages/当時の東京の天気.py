e36fcb0f83caabb354ca51a62cd3fce1807071d8
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2023-12-19 06:55:02 +0900

    Update 当時の東京の天気.py

--- ☆Webアプリ/streamlit/pages/当時の東京の天気.py
+++ ☆Webアプリ/streamlit/pages/当時の東京の天気.py
@@ -6,7 +6,7 @@ df = pd.read_csv("/mount/src/hatake4911/☆Webアプリ/csvファイル各種/�
 st.dataframe(df)
 
 # CSVファイルからデータを読み込む
-df = pd.read_csv("/mount/src/hatake4911/☆Webアプリ/csvファイル各種/２０２２年１０月の東京の天気.csv")
+df = pd.read_csv("/mount/src/hatake4911/☆Webアプリ/csvファイル各種/２０２２年１０月の東京の天気.csv",encoding='shift_jis')
 
 # 日ごとの平均気温と降水量を含む新しいデータフレームを作成
 df_plot = df[['日', '平均気温(℃)', '降水量(mm)合計']]
