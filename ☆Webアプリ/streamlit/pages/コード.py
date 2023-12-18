0c0a6cc42a15a250a70e52d11ecc2f84429378f4
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2023-12-19 06:07:05 +0900

    Update コード.py

--- ☆Webアプリ/streamlit/pages/コード.py
+++ ☆Webアプリ/streamlit/pages/コード.py
@@ -1,2 +1,12 @@
 import streamlit as st
 st.text("ページ2")
+st.text('main_app.pyで使用したコード')
+code='''
+
+'''
+st.code(code,language='python')
+st.text('当時の東京の天気.pyで使用したコード')
+code='''
+
+'''
+st.code(code,language='python')
