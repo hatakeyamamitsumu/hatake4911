327acadebaf2c4adcb862a27638efc164aa0c85b
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2023-12-19 20:30:28 +0900

    Update アップローダー.py

--- ☆Webアプリ/streamlit/pages/アップローダー.py
+++ ☆Webアプリ/streamlit/pages/アップローダー.py
@@ -1,1 +1,8 @@
+st.set_page_config(page_title='csvファイル',layout='centered')
+st.title('CSVファイルのアップロードと読み込み')
+uploaded_file=st.fole_uploader('file of CSV',type='csv',key='csv')
 
+if uploaded_file:
+  df=pd.read_csv(uploaded_file,encoding='utf-8')
+  st.markdown('#### dataframe')
+  st.dataframe(df)
