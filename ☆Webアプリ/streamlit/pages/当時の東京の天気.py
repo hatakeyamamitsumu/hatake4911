51d203fb9120eb0b6cec377e55ef59b80c1d483e
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2023-12-19 18:53:06 +0900

    Update 当時の東京の天気.py

--- ☆Webアプリ/streamlit/pages/当時の東京の天気.py
+++ ☆Webアプリ/streamlit/pages/当時の東京の天気.py
@@ -10,23 +10,8 @@ st.dataframe(df)
 df_plot_temp = df[['平均気温(℃)']]
 df_plot_rain = df[['降水量(mm)合計']]
 
-# 降水量(mm)合計で昇順にソート
-#df_plot_rain = df_plot_rain.sort_values(by='降水量(mm)合計', ascending=True)
-
 # Streamlitで折れ線グラフと縦棒グラフを描画
-st.bar_chart(df_plot_rain,y="降水量(mm)合計" ,use_container_width=True, height=400)
-st.line_chart(df_plot_temp,y='平均気温(℃)',use_container_width=True,height=400)
-
-
-#st.bar_chart(df_plot_rain[::-1], use_container_width=True, height=400)
-#st.bar_chart(df_plot_rain, use_container_width=True, height=400)
- # 降水量(mm)合計の縦棒グラフをMatplotlibで作成
-fig, ax = plt.subplots()
-ax.bar(df['降水量(mm)合計'])
-
-# グラフにラベルを追加
-ax.set_xlabel('日')
-ax.set_ylabel('降水量(mm)合計')
-
-# グラフをStreamlitに表示
-st.pyplot(fig)
+st.title("気温")
+st.line_chart(df_plot_temp,y='平均気温(℃)',use_container_width=True,height=300)
+st.title("気温")
+st.bar_chart(df_plot_rain,y="降水量(mm)合計" ,use_container_width=True, height=300)
