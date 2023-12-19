2f8cf281029b7dc4106e29e6bf1c4e7ffe836679
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2023-12-19 18:08:22 +0900

    Update 当時の東京の天気.py

--- ☆Webアプリ/streamlit/pages/当時の東京の天気.py
+++ ☆Webアプリ/streamlit/pages/当時の東京の天気.py
@@ -13,9 +13,10 @@ df_plot_rain = df[['降水量(mm)合計']]
 #df_plot_rain = df_plot_rain.sort_values(by='降水量(mm)合計', ascending=True)
 
 # Streamlitで折れ線グラフと縦棒グラフを描画
+st.bar_chart(df_plot_rain,y="降水量(mm)合計" ,use_container_width=True, height=400)
 st.line_chart(df_plot_temp,y='平均気温(℃)',use_container_width=True,height=400)
 
 
 #st.bar_chart(df_plot_rain[::-1], use_container_width=True, height=400)
 #st.bar_chart(df_plot_rain, use_container_width=True, height=400)
-st.bar_chart(df_plot_rain,y="降水量(mm)合計" ,use_container_width=True, height=400)
+
