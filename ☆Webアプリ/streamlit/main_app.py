e5a79bf796b093083329aea79fb4797bf2dcdaee
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2023-12-19 20:04:34 +0900

    Update main_app.py

--- ☆Webアプリ/streamlit/main_app.py
+++ ☆Webアプリ/streamlit/main_app.py
@@ -4,7 +4,8 @@ import datetime
 import pandas as pd
 import os
 df = pd.DataFrame
-
+cwd = os.getcwd()
+st.text(cwd)#このコードによってgithub上のフルパスを確認
 
 
 
@@ -13,8 +14,7 @@ st.caption('こんにちは！Hatakeです。')
 st.subheader('説明')
 st.text('簡易なWEBアプリ「streamlit」を使って何かやろうと考えています。\n'
        'よろしくお願いします。')
-cwd = os.getcwd()
-st.text(cwd)
+
 #フォルダ変更　　../images/my_image.jpg
 #写真
 st.text('こちらは2022年に東京に旅行した際の写真、動画です。')
