bb37a632ef7b10763f7c75115a5676ef15933739
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2023-12-19 18:17:09 +0900

    Update 当時の東京の天気.py

--- ☆Webアプリ/streamlit/pages/当時の東京の天気.py
+++ ☆Webアプリ/streamlit/pages/当時の東京の天気.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt
 st.write("２０２２年１０月の東京の天気（気象庁より取得）")
 st.write("https://www.data.jma.go.jp/stats/etrn/index.php")  
 df = pd.read_csv("/mount/src/hatake4911/☆Webアプリ/csvファイル各種/２０２２年１０月の東京の天気.csv", encoding='shift_jis',index_col="日")
@@ -19,4 +20,13 @@ st.line_chart(df_plot_temp,y='平均気温(℃)',use_container_width=True,height
 
 #st.bar_chart(df_plot_rain[::-1], use_container_width=True, height=400)
 #st.bar_chart(df_plot_rain, use_container_width=True, height=400)
+ # 降水量(mm)合計の縦棒グラフをMatplotlibで作成
+fig, ax = plt.subplots()
+ax.bar(df_plot_rain['日'], df_plot_rain['降水量(mm)合計'])
 
+# グラフにラベルを追加
+ax.set_xlabel('日')
+ax.set_ylabel('降水量(mm)合計')
+
+# グラフをStreamlitに表示
+st.pyplot(fig)
