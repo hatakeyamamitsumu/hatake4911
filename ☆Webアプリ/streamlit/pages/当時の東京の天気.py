c91ff21793e92aa5b51e5ed4a24f4bceffee581f
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2023-12-19 19:04:33 +0900

    Update 当時の東京の天気.py

--- ☆Webアプリ/streamlit/pages/当時の東京の天気.py
+++ ☆Webアプリ/streamlit/pages/当時の東京の天気.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
-st.write("２０２２年１０月の東京の天気（気象庁より取得）")
+st.write("２０２２年１０月の東京の天気（気象庁より取得.降雨、積雪の欠損値を0としております）")
 st.write("https://www.data.jma.go.jp/stats/etrn/index.php")  
 df = pd.read_csv("/mount/src/hatake4911/☆Webアプリ/csvファイル各種/２０２２年１０月の東京の天気.csv", encoding='shift_jis',index_col="日")
 st.dataframe(df, height=250)
