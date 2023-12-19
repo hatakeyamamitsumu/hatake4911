735e065e1f4e9eac831c3ca726d7333249f7bc7d
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2023-12-19 20:44:29 +0900

    Update アップローダー.py

--- ☆Webアプリ/streamlit/pages/アップローダー.py
+++ ☆Webアプリ/streamlit/pages/アップローダー.py
@@ -4,7 +4,7 @@ import openpyxl
 
 st.set_page_config(page_title='csvファイル',layout='centered')
 st.title('CSVファイルのアップロードと読み込み')
-uploaded_file=st.fole_uploader('file of CSV',type='csv',key='csv')
+uploaded_file=st.file_uploader('file of CSV',type='csv',key='csv')
 
 if uploaded_file:
   df=pd.read_csv(uploaded_file,encoding='utf-8')
