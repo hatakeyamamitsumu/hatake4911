3675c1add6c6a5d8f0a22301bc4845af2cd72bf6
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2023-12-19 21:08:07 +0900

    Update アップローダー.py

--- ☆Webアプリ/streamlit/pages/アップローダー.py
+++ ☆Webアプリ/streamlit/pages/アップローダー.py
@@ -4,7 +4,7 @@ import openpyxl
 
 st.set_page_config(page_title='csvファイル',layout='centered')
 st.title('CSVファイルのアップロードと読み込み1')
-uploaded_file=st.file_uploader('utf-8 CSV',type='csv',key='csv1')
+uploaded_file=st.file_uploader('utf-8 CSV',type='csv',key='csv1')#keyはアップローダーが複数あった時にそれぞれにつけるラベルのようなもの
 
 if uploaded_file:
   df=pd.read_csv(uploaded_file,encoding='utf-8')
@@ -13,7 +13,7 @@ if uploaded_file:
 
 
 st.title('CSVファイルのアップロードと読み込み2')
-uploaded_file=st.file_uploader('shift-jis CSV',type='csv',key='csv2')
+uploaded_file=st.file_uploader('shift-jis CSV',type='csv',key='csv2')#keyはアップローダーが複数あった時にそれぞれにつけるラベルのようなもの
 
 if uploaded_file:
   df=pd.read_csv(uploaded_file,encoding='shift-jis')
