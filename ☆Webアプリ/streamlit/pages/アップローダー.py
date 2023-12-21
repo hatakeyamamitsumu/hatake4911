97abf723e262af0a9fb31cf7bbec19aec66968c8
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2023-12-22 02:43:15 +0900

    Update アップローダー.py

--- ☆Webアプリ/streamlit/pages/アップローダー.py
+++ ☆Webアプリ/streamlit/pages/アップローダー.py
@@ -1,69 +1,46 @@
 import streamlit as st
 import pandas as pd
-import openpyxl
 import matplotlib.pyplot as plt
 
-st.set_page_config(page_title='csvファイル',layout='centered')
+st.set_page_config(page_title='csvファイル', layout='centered')
+
+# CSVファイルのアップロードと読み込み
+def load_csv_and_plot(file_uploader_key):
+    uploaded_file = st.file_uploader('CSVファイル', type='csv', key=file_uploader_key)
+    
+    if uploaded_file:
+        try:
+            df = pd.read_csv(uploaded_file, encoding='utf-8')
+            st.markdown('#### DataFrame')
+            st.dataframe(df)
+
+            # Create a multiselect dropdown for choosing columns
+            selected_columns = st.multiselect("Select Columns for Plotting", df.columns)
+
+            # 日ごとの選択された列を含む新しいデータフレームを作成
+            df_selected_columns = df[selected_columns]
+
+            # データを共有するための共通の軸を作成
+            fig, ax1 = plt.subplots()
+
+            # プロットを追加
+            for column in selected_columns:
+                color = 'tab:red'  # You can choose different colors for each line
+                ax1.set_xlabel('day')
+                ax1.set_ylabel(column, color=color)
+                ax1.plot(df.index, df_selected_columns[column], color=color, label=column)
+                ax1.tick_params(axis='y', labelcolor=color)
+
+            # グラフを表示
+            ax1.legend(loc='upper left', bbox_to_anchor=(1, 1))
+            st.pyplot(fig)
+            
+        except Exception as e:
+            st.error(f"エラー: {str(e)}")
+
+# 2回呼び出しているので、関数を使ってコードの重複を削減
 st.title('CSVファイルのアップロードと読み込み1')
-uploaded_file=st.file_uploader('utf-8 CSV',type='csv',key='csv1')#keyはアップローダーが複数あった時にそれぞれにつけるラベルのようなもの
-
-if uploaded_file:
-  df1=pd.read_csv(uploaded_file,encoding='utf-8')
-  st.markdown('#### dataframe')
-  st.dataframe(df1)
-
-
-
-  # Create a multiselect dropdown for choosing columns
-  selected_columns = st.multiselect("Select Columns for Plotting", df1.columns)
-
-  # 日ごとの選択された列を含む新しいデータフレームを作成
-  df_selected_columns = df1[selected_columns]
-
-  # データを共有するための共通の軸を作成
-  fig, ax1 = plt.subplots()
-
-  # プロットを追加
-  for column in selected_columns:
-      color = 'tab:red'  # You can choose different colors for each line
-      ax1.set_xlabel('day')
-      ax1.set_ylabel(column, color=color)
-      ax1.plot(df1.index, df_selected_columns[column], color=color, label=column)
-      ax1.tick_params(axis='y', labelcolor=color)
-
-      # グラフを表示
-      ax1.legend(loc='upper left', bbox_to_anchor=(1, 1))
-      st.pyplot(fig)
-
-
-
-
+load_csv_and_plot('csv1')
 
 st.title('CSVファイルのアップロードと読み込み2')
-uploaded_file=st.file_uploader('shift-jis CSV',type='csv',key='csv2')#keyはアップローダーが複数あった時にそれぞれにつけるラベルのようなもの
-
-if uploaded_file:
-  df2=pd.read_csv(uploaded_file,encoding='shift-jis')
-  st.markdown('#### dataframe')
-  st.dataframe(df2)
-
-  # Create a multiselect dropdown for choosing columns
-  selected_columns = st.multiselect("Select Columns for Plotting", df2.columns)
-
-  # 日ごとの選択された列を含む新しいデータフレームを作成
-  df_selected_columns = df2[selected_columns]
-
-  # データを共有するための共通の軸を作成
-  fig, ax1 = plt.subplots()
-
-  # プロットを追加
-  for column in selected_columns:
-      color = 'tab:red'  # You can choose different colors for each line
-      ax1.set_xlabel('day')
-      ax1.set_ylabel(column, color=color)
-      ax1.plot(df2.index, df_selected_columns[column], color=color, label=column)
-      ax1.tick_params(axis='y', labelcolor=color)
-
-    # グラフを表示
-    ax1.legend(loc='upper left', bbox_to_anchor=(1, 1))
-    st.pyplot(fig)
+load_csv_and_plot('csv2')
