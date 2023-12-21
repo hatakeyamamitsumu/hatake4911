cc302b2e2874bc4b823518078754921b7f0e312b
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2023-12-22 02:17:53 +0900

    Update アップローダー.py

--- ☆Webアプリ/streamlit/pages/アップローダー.py
+++ ☆Webアプリ/streamlit/pages/アップローダー.py
@@ -8,7 +8,7 @@ st.title('CSVファイルのアップロードと読み込み1')
 uploaded_file=st.file_uploader('utf-8 CSV',type='csv',key='csv1')#keyはアップローダーが複数あった時にそれぞれにつけるラベルのようなもの
 
 if uploaded_file:
-  df1=pd.read_csv(uploaded_file,encoding=None)
+  df1=pd.read_csv(uploaded_file,encoding='utf-8')
   st.markdown('#### dataframe')
   st.dataframe(df1)
 
