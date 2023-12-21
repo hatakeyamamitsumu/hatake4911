c41211ed965648353c02badabae879dd1d8691c0
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2023-12-22 05:25:51 +0900

    Update 画像を見て投票.py

--- ☆Webアプリ/streamlit/pages/画像を見て投票.py
+++ ☆Webアプリ/streamlit/pages/画像を見て投票.py
@@ -3,3 +3,8 @@ from PIL import Image
 import datetime
 import pandas as pd
 import os
+import plotly.graph_objects as go
+from st_clickable_images import clickable_images
+import base64
+import pickle
+from io import BytesIO
