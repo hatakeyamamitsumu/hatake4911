a450505df262572c645641879f52fe2cb5c695c9
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2023-12-30 20:02:12 +0900

    Update 立体散布図.py

--- ☆Webアプリ/streamlit/pages/立体散布図.py
+++ ☆Webアプリ/streamlit/pages/立体散布図.py
@@ -8,6 +8,10 @@ def load_data(file_path, encoding):
     data = pd.read_csv(file_path, encoding=encoding)
     return data
 
+def plot_3d_scatter(data, x_col, y_col, z_col, index_col):
+    fig = px.scatter_3d(data, x=x_col, y=y_col, z=z_col, text=index_col)
+    st.plotly_chart(fig)
+
 def main():
     st.title('3D Scatter Plot with Streamlit')
     
@@ -28,5 +32,17 @@ def main():
             else:
                 st.warning('Please select exactly 3 columns for the 3D Scatter Plot.')
 
+        if uploaded_file_shiftjis is not None:
+            data_shiftjis = load_data(uploaded_file_shiftjis, encoding='shift-jis')
+            selected_columns_shiftjis = st.multiselect('Select columns for 3D Scatter Plot', data_shiftjis.columns)
+            
+            if len(selected_columns_shiftjis) == 3:
+                index_col_shiftjis = st.selectbox('Select the index column for labels', data_shiftjis.columns)
+                st.write('### 3D Scatter Plot (Shift-JIS):')
+                plot_3d_scatter(data_shiftjis, selected_columns_shiftjis[0], selected_columns_shiftjis[1], selected_columns_shiftjis[2], index_col_shiftjis)
+            else:
+                st.warning('Please select exactly 3 columns for the 3D Scatter Plot.')
+
 if __name__ == '__main__':
     main()
+
