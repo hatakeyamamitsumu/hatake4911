ad5500079bc6b51dbad29a41c7a1a6ac18d6f405
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2023-12-30 20:54:01 +0900

    Update 動画の背景を切り抜き.py

--- ☆Webアプリ/streamlit/pages/動画の背景を切り抜き.py
+++ ☆Webアプリ/streamlit/pages/動画の背景を切り抜き.py
@@ -6,9 +6,9 @@ import base64
 
 st.set_page_config(layout="wide", page_title="Image Background Remover")
 
-st.write("## Remove background from your image")
+st.write("## 写真の背景を切り抜き")
 st.write(
-    ":dog: Try uploading an image to watch the background magically removed. Full quality images can be downloaded from the sidebar. This code is open source and available [here](https://github.com/tyler-simons/BackgroundRemoval) on GitHub. Special thanks to the [rembg library](https://github.com/danielgatis/rembg) :grin:"
+    "背景を切り抜きたい写真を、左のウインドウからアップロードしてください＜＜"
 )
 st.sidebar.write("## Upload and download :gear:")
 
