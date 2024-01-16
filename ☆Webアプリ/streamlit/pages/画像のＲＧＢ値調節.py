c4f9fdcef5740fff0219bf6252d806c72355c023
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-01-16 22:41:42 +0900

    Update 画像のＲＧＢ値調節.py

--- ☆Webアプリ/streamlit/pages/画像のＲＧＢ値調節.py
+++ ☆Webアプリ/streamlit/pages/画像のＲＧＢ値調節.py
@@ -77,7 +77,14 @@ if uploaded_file is not None:
     if st.button("画像をダウンロードしますか？"):
         # Pillowで画像を作成し、バイトデータに変換
         modified_image_bytes = io.BytesIO()
-        modified_image.save(modified_image_bytes, format='JPEG')
+
+        if apply_canny:
+            # Convert Canny Edge NumPy array to PIL Image
+            modified_image_canny = Image.fromarray(modified_image_rgb)
+            modified_image_canny.save(modified_image_bytes, format='JPEG')
+        else:
+            modified_image.save(modified_image_bytes, format='JPEG')
 
         # ダウンロードボタンに渡す
-        st.download_button("ダウンロード", modified_image_bytes.getvalue(), file_name="modified_image.jpg", key="download")
+        st.download_button("ダウンロード", modified_image
+
