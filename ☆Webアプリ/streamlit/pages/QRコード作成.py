b17ec8800387805fe3a318c2f8cdf6cf8985bf36
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-01-23 08:56:53 +0900

    Update QRコード作成.py

--- ☆Webアプリ/streamlit/pages/QRコード作成.py
+++ ☆Webアプリ/streamlit/pages/QRコード作成.py
@@ -2,25 +2,26 @@ import streamlit as st
 import qrcode
 from PIL import Image
 import io
-import numpy as np
+
 # ユーザーにデータを入力させる
 data = st.text_input("QRコードにしたい文字列を入力してください:")
 
 # データが入力されていればQRコードを作成
 if data:
     # QRコードを作成
-    _qr_img = qrcode.make(data)
+    qr_img = qrcode.make(data)
+    
     # 画像をファイルとして保存
-    _qr_img.save("QR.png")
-    img=Image.open("QR.png")
+    qr_img.save("QR.png")
+    
+    # Streamlitで画像を表示
+    img = Image.open("QR.png")
     st.image(img)
-
-
 
     # ダウンロードボタンを表示
     st.download_button(
         label="QRコードをダウンロード",
-        data=img,
+        data="QR.png",  # ファイルパスを指定
         key="download_qr_button"
     )
 else:
