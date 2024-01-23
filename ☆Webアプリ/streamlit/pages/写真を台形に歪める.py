224208d67c2a912bbb29263cd05f811f9f0753d0
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-01-23 22:28:44 +0900

    Update 写真を台形に歪める.py

--- ☆Webアプリ/streamlit/pages/写真を台形に歪める.py
+++ ☆Webアプリ/streamlit/pages/写真を台形に歪める.py
@@ -1,17 +1,16 @@
 import streamlit as st
 import cv2
-import matplotlib.pyplot as plt
 import numpy as np
 
-def perspective_transform(img_path, src_pts, dst_pts):
-    # 画像の読み込み
-    img = cv2.imread(img_path)
+def perspective_transform(img, src_pts, dst_pts):
+    # 画像のサイズを取得
+    height, width, _ = img.shape
 
     # 台形変換の行列を計算
     matrix = cv2.getPerspectiveTransform(src_pts, dst_pts)
 
     # 台形変換を適用
-    result = cv2.warpPerspective(img, matrix, (img.shape[1], img.shape[0]))
+    result = cv2.warpPerspective(img, matrix, (width, height))
 
     return result
 
@@ -23,18 +22,21 @@ def main():
 
     if uploaded_file is not None:
         # デフォルトの座標を設定
-        default_src_pts = np.array([[0, 0], [uploaded_file.width, 0], [uploaded_file.width, uploaded_file.height], [0, uploaded_file.height]], dtype=np.float32)
-        default_dst_pts = np.array([[uploaded_file.width * 0.25, uploaded_file.width*0.2],
-                                    [uploaded_file.width * 0.75, uploaded_file.width*0.1],
-                                    [uploaded_file.width, uploaded_file.height*0.5],
-                                    [0, uploaded_file.height*0.8]], dtype=np.float32)
+        default_src_pts = np.array([[0, 0], [width, 0], [width, height], [0, height]], dtype=np.float32)
+        default_dst_pts = np.array([[width * 0.25, width*0.2],
+                                    [width * 0.75, width*0.1],
+                                    [width, height*0.5],
+                                    [0, height*0.8]], dtype=np.float32)
 
         # スライダーで調節する座標を取得
-        src_pts = st.slider("変換前の4点の座標", 0.0, uploaded_file.width, default_src_pts)
-        dst_pts = st.slider("変換後の4点の座標", 0.0, uploaded_file.width, default_dst_pts)
+        src_pts = st.slider("変換前の4点の座標", 0.0, width, default_src_pts)
+        dst_pts = st.slider("変換後の4点の座標", 0.0, width, default_dst_pts)
+
+        # アップロードされた画像を読み込み
+        img = cv2.imdecode(np.fromstring(uploaded_file.read(), np.uint8), cv2.IMREAD_COLOR)
 
         # 画像を変換
-        transformed_image = perspective_transform(uploaded_file, src_pts, dst_pts)
+        transformed_image = perspective_transform(img, src_pts, dst_pts)
 
         # オリジナルと変換後の画像を表示
         st.image(cv2.cvtColor(transformed_image, cv2.COLOR_BGR2RGB), caption='Perspective Transformed Image', use_column_width=True)
