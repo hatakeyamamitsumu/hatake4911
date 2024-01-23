d315be36d2878bb6b836d4dbcd74332d6b917379
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-01-23 22:31:31 +0900

    Update 写真を台形に歪める.py

--- ☆Webアプリ/streamlit/pages/写真を台形に歪める.py
+++ ☆Webアプリ/streamlit/pages/写真を台形に歪める.py
@@ -22,18 +22,18 @@ def main():
 
     if uploaded_file is not None:
         # アップロードされた画像を読み込み
-        img = cv2.imdecode(np.fromstring(uploaded_file.read(), np.uint8), cv2.IMREAD_COLOR)
+        img = cv2.imdecode(np.frombuffer(uploaded_file.read(), np.uint8), cv2.IMREAD_COLOR)
 
         # デフォルトの座標を設定
-        default_src_pts = np.array([[0, 0], [width, 0], [width, height], [0, height]], dtype=np.float32)
-        default_dst_pts = np.array([[width * 0.25, width*0.2],
-                                    [width * 0.75, width*0.1],
-                                    [width, height*0.5],
-                                    [0, height*0.8]], dtype=np.float32)
+        default_src_pts = np.array([[0, 0], [img.shape[1], 0], [img.shape[1], img.shape[0]], [0, img.shape[0]]], dtype=np.float32)
+        default_dst_pts = np.array([[img.shape[1] * 0.25, img.shape[1] * 0.2],
+                                    [img.shape[1] * 0.75, img.shape[1] * 0.1],
+                                    [img.shape[1], img.shape[0] * 0.5],
+                                    [0, img.shape[0] * 0.8]], dtype=np.float32)
 
         # スライダーで調節する座標を取得
-        src_pts = st.slider("変換前の4点の座標", 0.0, width, default_src_pts)
-        dst_pts = st.slider("変換後の4点の座標", 0.0, width, default_dst_pts)
+        src_pts = st.slider("変換前の4点の座標", 0.0, img.shape[1], default_src_pts)
+        dst_pts = st.slider("変換後の4点の座標", 0.0, img.shape[1], default_dst_pts)
 
         # 画像を変換
         transformed_image = perspective_transform(img, src_pts, dst_pts)
@@ -47,4 +47,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
