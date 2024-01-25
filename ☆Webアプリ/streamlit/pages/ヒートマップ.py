dc9ae55ee38bdd999ab2870fe9fa5e47756f0752
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-01-26 08:16:02 +0900

    Update ヒートマップ.py

--- ☆Webアプリ/streamlit/pages/ヒートマップ.py
+++ ☆Webアプリ/streamlit/pages/ヒートマップ.py
@@ -8,9 +8,9 @@ def read_csv(uploaded_file, encoding=None):
 def colorize_column(df, col_idx, color_values):
     col_name = df.columns[col_idx]
     if pd.api.types.is_numeric_dtype(df[col_name]):
+        rank_col = df[col_name].rank(method='first', ascending=False) * 5
         for i, color in enumerate(color_values):
-            rank_col = df[col_name].rank(method='first', ascending=False) * 5
-            df = df.style.applymap(lambda x: f'background-color: rgb({max(0, 255 - color)}, 0, 0)', subset=pd.IndexSlice[0:i, col_name])
+            df[rank_col <= i] = df.style.applymap(lambda x: f'background-color: rgb({max(0, 255 - color)}, 0, 0)', subset=pd.IndexSlice[:, col_name])
     return df
 
 # ページのタイトル
@@ -28,7 +28,7 @@ if uploaded_file_utf8 is not None:
         df_utf8 = colorize_column(df_utf8, col_idx, color_values)
 
     # 表示
-    st.dataframe(df_utf8.data)
+    st.dataframe(df_utf8)
 
 # Shift-JIS用アップローダー
 uploaded_file_shift_jis = st.file_uploader("Shift-JISエンコーディングのCSVファイルをアップロードしてください", type=["csv"])
@@ -42,5 +42,5 @@ if uploaded_file_shift_jis is not None:
         df_shift_jis = colorize_column(df_shift_jis, col_idx, color_values)
 
     # 表示
-    st.dataframe(df_shift_jis.data)
+    st.dataframe(df_shift_jis)
 
