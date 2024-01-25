20400f06dc3ee168faf5166b6e799bd50012736c
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-01-26 08:14:06 +0900

    Update ヒートマップ.py

--- ☆Webアプリ/streamlit/pages/ヒートマップ.py
+++ ☆Webアプリ/streamlit/pages/ヒートマップ.py
@@ -10,7 +10,9 @@ def colorize_column(df, col_idx, color_values):
     if pd.api.types.is_numeric_dtype(df[col_name]):
         for i, color in enumerate(color_values):
             df[col_name] = df[col_name].sort_values(ascending=False).rank(method='first', ascending=False) * 5
-            df.style.applymap(lambda x: f'background-color: rgb({max(0, 255 - color)}, 0, 0)', subset=pd.IndexSlice[0:i, col_name])
+            df = df.style.applymap(lambda x: f'background-color: rgb({max(0, 255 - color)}, 0, 0)', subset=pd.IndexSlice[0:i, col_name])
+
+    return df
 
 # ページのタイトル
 st.title("CSVデータの数値列を着色表示")
@@ -24,7 +26,7 @@ if uploaded_file_utf8 is not None:
     # 列ごとに処理
     for col_idx in range(df_utf8.shape[1]):
         color_values = range(255, -1, -5)
-        colorize_column(df_utf8, col_idx, color_values)
+        df_utf8 = colorize_column(df_utf8, col_idx, color_values)
 
     # 表示
     st.dataframe(df_utf8)
@@ -38,7 +40,7 @@ if uploaded_file_shift_jis is not None:
     # 列ごとに処理
     for col_idx in range(df_shift_jis.shape[1]):
         color_values = range(255, -1, -5)
-        colorize_column(df_shift_jis, col_idx, color_values)
+        df_shift_jis = colorize_column(df_shift_jis, col_idx, color_values)
 
     # 表示
     st.dataframe(df_shift_jis)
