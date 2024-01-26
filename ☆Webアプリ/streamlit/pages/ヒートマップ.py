35d8728dad97a18edd891fb0820a6ffb56ff4ac5
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-01-26 10:55:51 +0900

    Update ヒートマップ.py

--- ☆Webアプリ/streamlit/pages/ヒートマップ.py
+++ ☆Webアプリ/streamlit/pages/ヒートマップ.py
@@ -1,14 +1,12 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
 
 def read_csv(uploaded_file, encoding):
     df = pd.read_csv(uploaded_file, encoding=encoding)
     return df
 
-def highlight_cells(df):
+def color_cells(df):
     styled_df = df.copy()
     num_cols = df.shape[1]
 
@@ -19,13 +17,13 @@ def highlight_cells(df):
                 break
 
             color = f'rgb({255 - j * 10}, 0, 0)'
-            idx_to_highlight = selected_col.index[j - 1]
-            styled_df.at[idx_to_highlight, i] = f'<mark style="background-color: {color}">{styled_df.at[idx_to_highlight, i]}</mark>'
+            idx_to_color = selected_col.index[j - 1]
+            styled_df.at[idx_to_color, i] = f'<mark style="background-color: {color}">{styled_df.at[idx_to_color, i]}</mark>'
 
     return styled_df
 
 # ページのタイトル
-st.title("CSVデータベースのセルを着色で強調表示")
+st.title("CSVデータのセルを着色で強調表示")
 
 # UTF-8用アップローダー
 uploaded_file_utf8 = st.file_uploader("UTF-8エンコーディングのCSVファイルをアップロードしてください", type=["csv"])
@@ -33,39 +31,16 @@ uploaded_file_utf8 = st.file_uploader("UTF-8エンコーディングのCSVファ
 # Shift-JIS用アップローダー
 uploaded_file_shift_jis = st.file_uploader("Shift-JISエンコーディングのCSVファイルをアップロードしてください", type=["csv"])
 
-# UTF-8データベースの処理
+# UTF-8データの処理
 if uploaded_file_utf8 is not None:
-    st.subheader("UTF-8データベース")
+    st.subheader("UTF-8データフレーム")
     df_utf8 = read_csv(uploaded_file_utf8, 'utf-8')
+    st.dataframe(color_cells(df_utf8), unsafe_allow_html=True)
 
-    # Matplotlib colormap
-    matplot_color = df_utf8.style.background_gradient(cmap='viridis')
-    st.write(matplot_color)
-
-    # Seaborn colorpalette
-    cm = sns.light_palette("green", as_cmap=True)
-    sns_color = df_utf8.style.background_gradient(cmap=cm)
-    st.write(sns_color)
-
-    # Seaborn colorpalette + nanを黒にする
-    sns_color_nan = df_utf8.style.background_gradient(cmap=cm).highlight_null('black')
-    st.write(sns_color_nan)
-
-# Shift-JISデータベースの処理
+# Shift-JISデータの処理
 if uploaded_file_shift_jis is not None:
-    st.subheader("Shift-JISデータベース")
+    st.subheader("Shift-JISデータフレーム")
     df_shift_jis = read_csv(uploaded_file_shift_jis, 'shift-jis')
+    st.dataframe(color_cells(df_shift_jis), unsafe_allow_html=True)
 
-    # Matplotlib colormap
-    matplot_color = df_shift_jis.style.background_gradient(cmap='viridis')
-    st.write(matplot_color)
-
-    # Seaborn colorpalette
-    cm = sns.light_palette("green", as_cmap=True)
-    sns_color = df_shift_jis.style.background_gradient(cmap=cm)
-    st.write(sns_color)
-
-    # Seaborn colorpalette + nanを黒にする
-    sns_color_nan = df_shift_jis.style.background_gradient(cmap=cm).highlight_null('black')
-    st.write(sns_color_nan)
 
