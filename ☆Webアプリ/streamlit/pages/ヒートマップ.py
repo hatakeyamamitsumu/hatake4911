176c3c7a957159d16a2caa0ab43587ab99b1ecd6
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-01-26 10:24:42 +0900

    Update ヒートマップ.py

--- ☆Webアプリ/streamlit/pages/ヒートマップ.py
+++ ☆Webアプリ/streamlit/pages/ヒートマップ.py
@@ -6,27 +6,28 @@ def read_csv(uploaded_file, encoding):
     df = pd.read_csv(uploaded_file, encoding=encoding)
     return df
 
+def highlight_cells(df):
+    styled_df = df.copy()
+    num_cols = df.shape[1]
+
+    for i in range(num_cols):
+        selected_col = df.iloc[:, i].sort_values(ascending=False)
+        for j in range(1, 26):
+            if j == 25:
+                break
+
+            color = f'rgb({255 - j * 10}, 0, 0)'
+            idx_to_highlight = selected_col.index[j - 1]
+            styled_df.at[idx_to_highlight, i] = f'background-color: {color}'
+
+    return styled_df
+
 # ページのタイトル
 st.title("CSVデータベースのセルを着色で強調表示")
 
 # ファイルアップローダー
 uploaded_file_utf8 = st.file_uploader("UTF-8エンコーディングのCSVファイルをアップロードしてください", type=["csv"])
 uploaded_file_shift_jis = st.file_uploader("Shift-JISエンコーディングのCSVファイルをアップロードしてください", type=["csv"])
-
-def highlight_cells(df):
-    styled_df = df.copy()
-    num_rows, num_cols = df.shape
-
-    for i in range(1, 26):
-        if i == 25:
-            break
-
-        color = f'rgb({255 - i * 10}, 0, 0)'
-        for col_idx in range(num_cols):
-            rank_series = df.iloc[:, col_idx].rank(ascending=False, method='min')
-            styled_df.iloc[:, col_idx] = np.where(rank_series == i, f'background-color: {color}', '')
-
-    return styled_df
 
 # UTF-8データベースの処理
 if uploaded_file_utf8 is not None:
@@ -39,3 +40,4 @@ if uploaded_file_shift_jis is not None:
     st.subheader("Shift-JISデータベース")
     df_shift_jis = read_csv(uploaded_file_shift_jis, 'shift-jis')
     st.dataframe(highlight_cells(df_shift_jis))
+
