b7655e9d9a5e4b6c976403b5029dd2728c8f2925
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-01-29 19:00:40 +0900

    Create おすすめリンク.py

---
+++ ☆Webアプリ/streamlit/pages/おすすめリンク.py
@@ -0,0 +1,3 @@
+link = '[<span class="hljs-string">Chemesim</span>](<span class="hljs-link">https://williammer.github.io/works/shodo/</span>)'
+st.sidebar.markdown(link, unsafe<span class="hljs-emphasis">_allow_</span>html=True)
+st.sidebar.write('書道ツールです')
