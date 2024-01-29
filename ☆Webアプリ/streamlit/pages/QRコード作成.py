e32d0a5fe68d135b3e6b39ba2b10afd745f851c6
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-01-30 00:08:53 +0900

    Update QRコード作成.py

--- ☆Webアプリ/streamlit/pages/QRコード作成.py
+++ ☆Webアプリ/streamlit/pages/QRコード作成.py
@@ -24,7 +24,7 @@ def add_text_to_qr(img, text):
 
 # ユーザーにデータを入力させ、QRコードのサイズとテキストを調節可能にする
 data = st.text_input("QRコードにする文字列を入力してください")
-qr_size = st.slider("QRコードのサイズを調整してください", min_value=100, max_value=1000, value=500)
+qr_size = st.slider("QRコードの余白を調整してください", min_value=100, max_value=1000, value=500)
 custom_text = st.text_input("QRコードに添える説明書き(アルファベットと数字のみ)")
 
 # データが入力されていればQRコードを作成
