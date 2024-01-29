878394bbfcf1079c728b89e0b9d3020ba9ac4fd7
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-01-29 19:23:16 +0900

    Update おすすめリンク.py

--- ☆Webアプリ/streamlit/pages/おすすめリンク.py
+++ ☆Webアプリ/streamlit/pages/おすすめリンク.py
@@ -5,3 +5,9 @@ link_str = "[The Shodo](https://williammer.github.io/works/shodo/)"
 # Markdownを表示。
 st.markdown(link_str, unsafe_allow_html=True)
 st.text('簡単な書道ができるフリーソフトです。')
+
+# ハイパーリンクを表示するMarkdown文字列
+link_str = "[The Shodo](https://qrcode.onl.jp/)"
+# Markdownを表示。
+st.markdown(link_str, unsafe_allow_html=True)
+st.text('QRコードリーダーです。')
