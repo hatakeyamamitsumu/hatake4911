c40774b05643dca423302d40ac88052dbe604068
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-01-29 19:09:23 +0900

    Update おすすめリンク.py

--- ☆Webアプリ/streamlit/pages/おすすめリンク.py
+++ ☆Webアプリ/streamlit/pages/おすすめリンク.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 # ハイパーリンクを表示するMarkdown文字列
-link_str = "[書道ツールです](https://williammer.github.io/works/shodo/)"
+link_str = "[The Shodo](https://williammer.github.io/works/shodo/)"
 # Markdownを表示
 st.markdown(link_str, unsafe_allow_html=True)
-
+st.text('簡単な書道ができるフリーソフトです。')
