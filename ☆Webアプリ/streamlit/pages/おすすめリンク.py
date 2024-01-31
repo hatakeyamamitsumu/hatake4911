fa54a4a3f06f33f75283bae03197077276d8fae6
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-01-31 11:37:31 +0900

    Update おすすめリンク.py

--- ☆Webアプリ/streamlit/pages/おすすめリンク.py
+++ ☆Webアプリ/streamlit/pages/おすすめリンク.py
@@ -11,6 +11,7 @@ link_str = "[QRコード読み取り](https://qrcode.onl.jp/)"
 # Markdownを表示。
 st.markdown(link_str, unsafe_allow_html=True)
 st.text('QRコードリーダーです。')
+
 https://1drv.ms/x/c/25c3642a3103cdcb/Ecn7kcD0QC1EqpBxqe0ZpgEBNSiFlTUkDm0G1uj8S3mzFw?e=Zthahb
 # ハイパーリンクを表示するMarkdown文字列
 link_str = "[自分のエクセルブック](https://1drv.ms/x/c/25c3642a3103cdcb/Ecn7kcD0QC1EqpBxqe0ZpgEBNSiFlTUkDm0G1uj8S3mzFw?e=Zthahb)"
