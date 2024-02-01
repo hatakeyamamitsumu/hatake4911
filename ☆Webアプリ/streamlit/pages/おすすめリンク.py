f932cdf53a57cc067c54cdf099609cfad0524323
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-02-01 23:57:40 +0900

    Update おすすめリンク.py

--- ☆Webアプリ/streamlit/pages/おすすめリンク.py
+++ ☆Webアプリ/streamlit/pages/おすすめリンク.py
@@ -2,22 +2,22 @@ import streamlit as st
 st.title("おすすめリンク")
 st.text('一部スマホではご利用いただけません。')
 # ハイパーリンクを表示するMarkdown文字列
-link_str = "[The Shodo](https://williammer.github.io/works/shodo/)"
+link_str = "(https://williammer.github.io/works/shodo/)"
 # Markdownを表示。
 st.markdown(link_str, unsafe_allow_html=True)
 st.text('簡単な書道ができるフリーソフトです。')
 
 # ハイパーリンクを表示するMarkdown文字列
-link_str = "[QRコード読み取り](https://qrcode.onl.jp/)"
+link_str = "(https://qrcode.onl.jp/)"
 # Markdownを表示。
 st.markdown(link_str, unsafe_allow_html=True)
 st.text('QRコードリーダーです。')
 
 # ハイパーリンクを表示するMarkdown文字列
-link_str = "[自分のエクセルブック](https://1drv.ms/x/c/25c3642a3103cdcb/EcOvbcbbK9ZAqtcuGtxvIKoB0CpKPBG5HYFYx05K9cEVRQ?e=cW1fFT)"
+link_str = "(https://1drv.ms/x/c/25c3642a3103cdcb/EcOvbcbbK9ZAqtcuGtxvIKoB0CpKPBG5HYFYx05K9cEVRQ?e=cW1fFT)"
 # Markdownを表示。
 st.markdown(link_str, unsafe_allow_html=True)
-st.text('自分のエクセルブックです。')
+st.text('マクロ等の挙動テスト用エクセルブック')
 
 # ハイパーリンクを表示するMarkdown文字列
 link_str = "(https://1drv.ms/f/c/25c3642a3103cdcb/EtW74Af8pZJEvbgsxfhCAgoBBufG7sLiDQJKDcu2UhWzNw?e=dy53br)"
