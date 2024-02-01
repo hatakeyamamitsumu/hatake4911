12a548e92444354fc9d80685e157b86516ea3f58
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-02-02 02:42:28 +0900

    Update 台形変換.py

--- ☆Webアプリ/streamlit/pages/台形変換.py
+++ ☆Webアプリ/streamlit/pages/台形変換.py
@@ -3,7 +3,8 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 
-def perspective_transform(img, src_pts, dst_pts):
+def perspective_transform(img, dst_pts):
+    src_pts = np.array([[0, 0], [img.shape[1], 0], [img.shape[1], img.shape[0]], [0, img.shape[0]]], dtype=np.float32)
     matrix = cv2.getPerspectiveTransform(src_pts, dst_pts)
     result = cv2.warpPerspective(img, matrix, (img.shape[1], img.shape[0]))
     return result
@@ -17,33 +18,21 @@ def main():
         # 画像の読み込み
         image = cv2.imdecode(np.fromstring(uploaded_file.read(), np.uint8), 1)
 
-        # ユーザーが操作するための4点の座標を設定する
-        src_pts = st.multiselect("Select source points", [(0, 0), (image.shape[1], 0), (image.shape[1], image.shape[0]), (0, image.shape[0])])
-
-        if len(src_pts) != 4:
-            st.warning("Please select exactly 4 source points.")
-            return
-
-        src_pts = np.array(src_pts, dtype=np.float32)
-
-        # ユーザーが操作するための変換後の4点の座標を設定する
-        dst_pts = st.multiselect("Select destination points", [(0, 0), (image.shape[1], 0), (image.shape[1], image.shape[0]), (0, image.shape[0])])
-
-        if len(dst_pts) != 4:
-            st.warning("Please select exactly 4 destination points.")
-            return
-
-        dst_pts = np.array(dst_pts, dtype=np.float32)
+        # 変換前、変換後の4点の座標を設定する
+        dst_pts = np.array([[image.shape[1] * 0.25, image.shape[1] * 0.2],
+                            [image.shape[1] * 0.75, image.shape[1] * 0.1],
+                            [image.shape[1], image.shape[0] * 0.5],
+                            [0, image.shape[0] * 0.8]], dtype=np.float32)
 
         # 台形変換を適用
-        result = perspective_transform(image, src_pts, dst_pts)
+        result = perspective_transform(image, dst_pts)
 
         # オリジナルと変換後の画像を表示
         st.image(cv2.cvtColor(image, cv2.COLOR_BGR2RGB), caption='Original Image', use_column_width=True)
         st.image(cv2.cvtColor(result, cv2.COLOR_BGR2RGB), caption='Perspective Transformed Image', use_column_width=True)
 
         # 変換後の画像をファイルとして保存
-        st.download_button(label="Download Transformed Image", data=cv2.imencode('.jpg', cv2.cvtColor(result, cv2.COLOR_BGR2RGB))[1].tobytes(), file_name='Perspective_Transformed_Image.jpg', mime='image/jpg')
+        st.download_button(label="Download Transformed Image", data= cv2.imencode('.jpg', cv2.cvtColor(result, cv2.COLOR_BGR2RGB))[1].tobytes(), file_name='Perspective_Transformed_Image.jpg', mime='image/jpg')
 
 if __name__ == '__main__':
     main()
