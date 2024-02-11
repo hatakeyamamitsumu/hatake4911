1c768a75a0deafc5894eee0d800e155d8617bd79
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-02-11 09:40:44 +0900

    Update ぼかし調節.py

--- ☆Webアプリ/streamlit/pages/ぼかし調節.py
+++ ☆Webアプリ/streamlit/pages/ぼかし調節.py
@@ -28,25 +28,6 @@ def fix_and_blend_images(original, cutout, blur_radius):
 
     return original_blurred
 
-def fix_image(upload, blur_radius):
-    image = Image.open(upload)
-    col1.write("Original Image :camera:")
-    col1.image(image)
-
-    # ぼかし効果を適用した元の画像を作成
-    original_blurred = image.filter(ImageFilter.GaussianBlur(blur_radius))
-
-    # 切り抜き処理
-    cutout = remove(image)
-
-    # 合成してぼかし効果を適用
-    blended_image = fix_and_blend_images(original_blurred, cutout, blur_radius)
-
-    col2.write("Fixed and Blurred Image :wrench:")
-    col2.image(blended_image)
-    st.sidebar.markdown("\n")
-    st.sidebar.download_button("ダウンロードボタン", convert_image(blended_image), "blended_image.png", "image/png")
-
 col1, col2 = st.columns(2)
 my_upload = st.sidebar.file_uploader("ここからアップロードしてください", type=["png", "jpg", "jpeg"])
 blur_radius = st.sidebar.slider("ぼかし具合", 0, 20, 5)  # You can adjust the min, max, and default values as needed.
@@ -58,3 +39,4 @@ if my_upload is not None:
         fix_image(upload=my_upload, blur_radius=blur_radius)
 else:
     fix_image("/mount/src/hatake4911/☆Webアプリ/画像/skytree.png", blur_radius=blur_radius)
+
