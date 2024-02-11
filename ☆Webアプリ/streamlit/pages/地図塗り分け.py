4a74dc9a6fddbe1b72cdb5bdc370d4497fd4532f
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-02-11 15:14:24 +0900

    Update 地図塗り分け.py

--- ☆Webアプリ/streamlit/pages/地図塗り分け.py
+++ ☆Webアプリ/streamlit/pages/地図塗り分け.py
@@ -1,30 +1,34 @@
 import streamlit as st
 import pandas as pd
-import plotly.express as px
+import geopandas as gpd
+import matplotlib.pyplot as plt
 
 # CSVファイルの読み込み
-data = pd.read_csv('/mount/src/hatake4911/☆Webアプリ/CSVファイル各種/都道府県を塗り分け用ＣＳＶ/都道府県別の博物館の数.CSV', index_col=0)
+data = pd.read_csv('都道府県別の博物館の数(2018).csv', index_col=0)
 
-# 最大の博物館の数を取得
-max_museums = data['データ'].max()
+# GeoJSON ファイルの読み込み
+geojson_url = 'https://raw.githubusercontent.com/niiyz/Japan-Geojson/master/geojson/prefectures.geojson'
+geojson_gdf = gpd.read_file(geojson_url)
 
-# 都道府県ごとの色データを作成
-color_data = {prefecture: int(255 * (1 - museums / max_museums)) for prefecture, museums in zip(data.index, data['データ'])}
+# データの結合
+merged_gdf = geojson_gdf.merge(data, left_on='name', right_index=True)
 
-# グラフのサイズを設定
-fig = px.choropleth(data, 
-                    geojson="https://raw.githubusercontent.com/codeforamerica/click_that_hood/master/public/data/japan.geojson",
-                    featureidkey="properties.name",
-                    locations=data.index,
-                    color=color_data,
-                    color_continuous_scale="reds",
-                    title="都道府県ごとの博物館の数に基づく色付け",
-                    labels={'color': '博物館の数'},
-                    width=800, height=600)
+# 描画
+fig, ax = plt.subplots(1, 1, figsize=(15, 10))
+merged_gdf.plot(column='データ', cmap='OrRd', linewidth=0.8, ax=ax, edgecolor='0.8', legend=True)
 
-# カラーバーを表示（博物館の数が多いほど赤くなる）
-fig.update_layout(coloraxis_colorbar=dict(title='博物館の数'))
+# カラーバーを追加
+cax = fig.add_axes([0.9, 0.5, 0.03, 0.3])
+sm = plt.cm.ScalarMappable(cmap='OrRd', norm=plt.Normalize(vmin=data['データ'].min(), vmax=data['データ'].max()))
+sm._A = []
+fig.colorbar(sm, cax=cax, label='博物館の数')
+
+# タイトルとラベル
+plt.title('都道府県ごとの博物館の数')
+plt.xlabel('経度')
+plt.ylabel('緯度')
 
 # Streamlit アプリの構築
 st.title("都道府県ごとの博物館の数に基づく色付け")
-st.plotly_chart(fig)
+st.pyplot(fig)
+
