5a1a5403811bb015e4f52d7c5125c8197639f6ea
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-02-13 09:16:42 +0900

    Update 文章を分析2.py

--- ☆Webアプリ/streamlit/pages/文章を分析2.py
+++ ☆Webアプリ/streamlit/pages/文章を分析2.py
@@ -61,7 +61,8 @@ def filter_and_download(text, filter_words, filter_condition):
         # 結果をCSVファイルとしてダウンロード
         csv_data = StringIO()
         result_df.to_csv(csv_data, index=False)
-        st.download_button(label="CSVファイルとしてダウンロード", data=csv_data.getvalue(), file_name="filtered_data.csv", key="download_button")
+        st.download_button(label="CSVファイルとしてダウンロード", data=csv_data.getvalue(), file_name="filtered_data.csv", key="download_button_unique_id")
+
     else:
         st.write(f"テキストに '{', '.join(filter_words)}' を含む行は見つかりませんでした。")
 
