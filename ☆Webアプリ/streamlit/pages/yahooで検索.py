b21f3e0880fa2feda7b065106128681bb58d307a
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-02-18 11:34:58 +0900

    Update yahooで検索.py

--- ☆Webアプリ/streamlit/pages/yahooで検索.py
+++ ☆Webアプリ/streamlit/pages/yahooで検索.py
@@ -34,32 +34,3 @@ st.write(df)
 
 
 
-# YahooニュースのWebページを解析する関数
-def parse_tamiya_news():
-    url = 'https://www.tamiya.com/japan/event/'
-    html = requests.get(url)
-    soup = BeautifulSoup(html.content, 'html.parser')
-    
-    # Yahooニュース内の最新トピックスをclass属性で検索（都度変更が必要）
-    topic2 = soup.find(class_='inner_')
-    
-    news_text2 = [i.text for i in topic2.find_all('a')]
-    news_link2 = [i.get('href') for i in topic2.find_all('a')]
-    
-    return news_text, news_link
-
-# Streamlitアプリケーションの開始
-st.title('見出し')
-
-# Yahooニュースを解析してデータを取得
-news_text, news_link = parse_tamiya_news()
-
-# 取得したデータをDataFrameに格納
-data = {'主要ニュース': news_text, 'リンク': news_link}
-df = pd.DataFrame(data)
-
-# データを表示する
-st.write('## ニュース一覧')
-st.write(df)
-
-inner_
