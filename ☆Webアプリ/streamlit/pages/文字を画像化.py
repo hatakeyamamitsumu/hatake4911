9fee4ce578b6b75bb4d7c65ab13ba37bd62610a9
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-02-20 04:06:46 +0900

    Update 文字を画像化.py

--- ☆Webアプリ/streamlit/pages/文字を画像化.py
+++ ☆Webアプリ/streamlit/pages/文字を画像化.py
@@ -13,7 +13,7 @@ def text_to_image(text, output_path):
 
     # 使用するフォントとテキストの設定
     font_size = 15
-    font = ImageFont.truetype("/mount/src/hatake4911/☆Webアプリ/フォントファイル/HGRPRE.TTC", font_size)  # フォントは適切なものを指定
+    font = ImageFont.truetype("/mount/src/hatake4911/☆Webアプリ/フォントファイル/HGRGM.TTC", font_size)  # フォントは適切なものを指定
     text_color = "black"
 
     # テキストを中央に配置
