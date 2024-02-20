b48f2a00d5eeba37fc63ff1a98a3008443d13460
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-02-20 22:59:22 +0900

    Create ヒートマップ.py

---
+++ ☆Webアプリ/streamlit/pages/ヒートマップ.py
@@ -0,0 +1,18 @@
+import streamlit as st
+import folium
+from folium import plugins
+
+# 中心座標(適当
+center = [32, 131]
+
+# ベースの地図作成
+m = folium.Map(center, zoom_start=12)
+
+# ヒートマップ層を地図に追加
+folium.plugins.HeatMap(
+    data=[center]  # 注意！ ２次元にして渡す
+).add_to(m)
+
+# Streamlit で地図を表示
+st.header("ヒートマップの例")
+st.pydeck_chart(m)
