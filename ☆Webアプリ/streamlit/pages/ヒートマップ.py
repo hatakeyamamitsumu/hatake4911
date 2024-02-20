2393f442f834cd7a769e23ce669c0bc63fbc4785
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-02-20 22:00:43 +0900

    Update ヒートマップ.py

--- ☆Webアプリ/streamlit/pages/ヒートマップ.py
+++ ☆Webアプリ/streamlit/pages/ヒートマップ.py
@@ -1,27 +1,18 @@
 import folium
-import pandas as pd
-from streamlit_folium import folium_static
-import streamlit as st
+from folium.plugins import HeatMap
 
 # 緯度と経度の情報
 latitudes = [35.652832, 35.658581, 35.660333]
 longitudes = [139.839478, 139.840439, 139.836389]
 
-# データの準備
-df = pd.DataFrame({'緯度': latitudes, '経度': longitudes})
-
-# データ型の確認と必要なら変換
-df['緯度'] = df['緯度'].astype('float')
-df['経度'] = df['経度'].astype('float')
-
-# サイドバーで半径の値を選択できるようにする
-radius = st.sidebar.slider('熱マップの半径', 10, 50, 15)
-
 # 地図の作成
-map = folium.Map(location=[latitudes[0], longitudes[0]], zoom_start=10)
+map_center = [sum(latitudes) / len(latitudes), sum(longitudes) / len(longitudes)]
+my_map = folium.Map(location=map_center, zoom_start=15)
 
-# 熱マップの生成
-heatmap = folium.HeatMap(data=df[['緯度', '経度']], radius=radius).add_to(map)
+# ヒートマップの生成
+heat_data = [[lat, lon] for lat, lon in zip(latitudes, longitudes)]
+HeatMap(heat_data).add_to(my_map)
 
 # 地図の表示
-folium_static(map)
+my_map.save("heatmap.html")
+
