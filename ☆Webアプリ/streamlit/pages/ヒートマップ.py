57f324aa4872c20f0a06dafd55e3bfaea28c02f0
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-02-20 21:54:27 +0900

    Update ヒートマップ.py

--- ☆Webアプリ/streamlit/pages/ヒートマップ.py
+++ ☆Webアプリ/streamlit/pages/ヒートマップ.py
@@ -4,14 +4,17 @@ import streamlit as st
 
 # データの準備 main/☆Webアプリ/CSVファイル各種/ヒートマップ用CSV/ヒートマップ用緯度経度情報.csv
 #/mount/src/hatake4911/☆Webアプリ/CSVファイル各種/ヒートマップ用CSV/ヒートマップ用緯度経度情報.csv
-df['緯度'] = df['緯度'].astype('float')
-df['経度'] = df['経度'].astype('float')
+
 # 緯度と経度の情報
 latitudes = [35.652832, 35.658581, 35.660333]
 longitudes = [139.839478, 139.840439, 139.836389]
 
 # データの準備
 df = pd.DataFrame({'緯度': latitudes, '経度': longitudes})
+
+# データ型の確認と必要なら変換（7行目をここへ移動）
+df['緯度'] = df['緯度'].astype('float')
+df['経度'] = df['経度'].astype('float')
 
 # サイドバーで半径の値を選択できるようにする
 radius = st.sidebar.slider('熱マップの半径', 10, 50, 15)
