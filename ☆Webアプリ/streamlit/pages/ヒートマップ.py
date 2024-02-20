1d3bda17076a3cead643839ac506aa627367811b
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-02-20 21:48:50 +0900

    Update ヒートマップ.py

--- ☆Webアプリ/streamlit/pages/ヒートマップ.py
+++ ☆Webアプリ/streamlit/pages/ヒートマップ.py
@@ -4,13 +4,19 @@ import streamlit as st
 
 # データの準備 main/☆Webアプリ/CSVファイル各種/ヒートマップ用CSV/ヒートマップ用緯度経度情報.csv
 #/mount/src/hatake4911/☆Webアプリ/CSVファイル各種/ヒートマップ用CSV/ヒートマップ用緯度経度情報.csv
-df = pd.read_csv("/mount/src/hatake4911/☆Webアプリ/CSVファイル各種/ヒートマップ用CSV/ヒートマップ用緯度経度情報.csv")
+
+# 緯度と経度の情報
+latitudes = [35.652832, 35.658581, 35.660333]
+longitudes = [139.839478, 139.840439, 139.836389]
+
+# データの準備
+df = pd.DataFrame({'緯度': latitudes, '経度': longitudes})
 
 # サイドバーで半径の値を選択できるようにする
 radius = st.sidebar.slider('熱マップの半径', 10, 50, 15)
 
 # 地図の作成
-map = folium.Map(location=[35.652832, 139.839478], zoom_start=10)
+map = folium.Map(location=[latitudes[0], longitudes[0]], zoom_start=10)
 
 # 熱マップの生成
 heatmap = folium.HeatMap(data=df[['緯度', '経度']], radius=radius).add_to(map)
