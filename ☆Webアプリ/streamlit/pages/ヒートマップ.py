100586851ba72f2a1b359c4969260468376c8db1
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-02-20 21:31:33 +0900

    Update ヒートマップ.py

--- ☆Webアプリ/streamlit/pages/ヒートマップ.py
+++ ☆Webアプリ/streamlit/pages/ヒートマップ.py
@@ -3,7 +3,10 @@ import pandas as pd
 import streamlit as st
 
 # データの準備
-df = pd.read_csv("data.csv")
+df = pd.read_csv("緯度,経度
+35.652832,139.839478
+35.658581,139.840439
+35.660333,139.836389")
 
 # サイドバーで半径の値を選択できるようにする
 radius = st.sidebar.slider('熱マップの半径', 10, 50, 15)
