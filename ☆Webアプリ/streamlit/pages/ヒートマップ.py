3cc8ac0178605ee0620d05ff69fb3c65f2b428cf
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-02-20 21:53:15 +0900

    Update ヒートマップ.py

--- ☆Webアプリ/streamlit/pages/ヒートマップ.py
+++ ☆Webアプリ/streamlit/pages/ヒートマップ.py
@@ -4,7 +4,8 @@ import streamlit as st
 
 # データの準備 main/☆Webアプリ/CSVファイル各種/ヒートマップ用CSV/ヒートマップ用緯度経度情報.csv
 #/mount/src/hatake4911/☆Webアプリ/CSVファイル各種/ヒートマップ用CSV/ヒートマップ用緯度経度情報.csv
-
+df['緯度'] = df['緯度'].astype('float')
+df['経度'] = df['経度'].astype('float')
 # 緯度と経度の情報
 latitudes = [35.652832, 35.658581, 35.660333]
 longitudes = [139.839478, 139.840439, 139.836389]
