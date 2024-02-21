fe00cad7cd2d70a9881b4c6968d0e8178997f786
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-02-21 12:45:10 +0900

    Update ヒートマップ2.py

--- ☆Webアプリ/streamlit/pages/ヒートマップ2.py
+++ ☆Webアプリ/streamlit/pages/ヒートマップ2.py
@@ -5,7 +5,7 @@ import folium
 from folium import plugins
 
 # Read data from CSV file CSVファイル各種/ヒートマップ地図用CSV/標高情報.csv
-data = pd.read_csv("/mount/src/hatake4911/☆Webアプリ/ヒートマップ地図用CSV/標高情報.csv")
+data = pd.read_csv("/mount/src/hatake4911/☆Webアプリ//CSVファイル各種/ヒートマップ地図用CSV/標高情報.csv")
 
 # Center of the map (you may adjust this based on your data)
 center = [data['緯度'].mean(), data['経度'].mean()]
