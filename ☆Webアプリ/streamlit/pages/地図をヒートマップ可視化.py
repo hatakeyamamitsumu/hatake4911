b118ff670ba62958c5c662f12a759fb4b98cdd9a
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-02-22 11:30:28 +0900

    Update 地図をヒートマップ可視化.py

--- ☆Webアプリ/streamlit/pages/地図をヒートマップ可視化.py
+++ ☆Webアプリ/streamlit/pages/地図をヒートマップ可視化.py
@@ -5,23 +5,17 @@ from streamlit_folium import folium_static
 import folium
 from folium import plugins
 
-# Specify the main folder path
-main_folder_path = "/mount/src/hatake4911/☆Webアプリ//CSVファイル各種/ヒートマップ地図用CSV/"
+# Specify the folder path
+folder_path = "/mount/src/hatake4911/☆Webアプリ//CSVファイル各種/ヒートマップ地図用CSV"
 
-# List all subfolders in the main folder
-subfolders = [f.path for f in os.scandir(main_folder_path) if f.is_dir()]
-
-# Allow the user to select a subfolder
-selected_subfolder = st.selectbox("サブフォルダを選択してください", subfolders)
-
-# List all files in the selected subfolder
-files_in_subfolder = [f for f in os.listdir(selected_subfolder) if f.endswith(".csv")]
+# List all CSV files in the folder
+csv_files = [f for f in os.listdir(folder_path) if f.endswith(".csv")]
 
 # Allow the user to select a CSV file
-selected_file = st.selectbox("CSVファイルを選択してください", files_in_subfolder)
+selected_file = st.selectbox("CSVファイルを選択してください", csv_files)
 
 # Construct the full file path
-csv_file_path = os.path.join(selected_subfolder, selected_file)
+csv_file_path = os.path.join(folder_path, selected_file)
 
 # Read data from the selected CSV file
 data = pd.read_csv(csv_file_path)
@@ -56,6 +50,7 @@ for index, row in data.iterrows():
 # Display the map using Streamlit
 st.header("ヒートマップ")
 folium_static(m)
+
 
 
 
