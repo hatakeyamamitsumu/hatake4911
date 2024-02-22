e0cadad8fdc31ebfa891a95b9d64a9506607402d
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-02-22 11:17:09 +0900

    Update 文章を分析2.py

--- ☆Webアプリ/streamlit/pages/文章を分析2.py
+++ ☆Webアプリ/streamlit/pages/文章を分析2.py
@@ -1,3 +1,34 @@
+import streamlit as st
+import pandas as pd
+from io import StringIO
+from docx import Document
+import re
+
+def count_words(text):
+    words = text.split()
+    word_counts = dict(pd.Series(words).value_counts())
+    return word_counts
+
+def read_word_file(file):
+    doc = Document(file)
+    text = ""
+    for paragraph in doc.paragraphs:
+        text += paragraph.text + "\n"
+    return text
+
+def filter_katakana(text):
+    katakana_lines = [line for line in text.split('\n') if any('\u30A1' <= char <= '\u30F6' for char in line)]
+    return katakana_lines
+
+def filter_numbers(text):
+    number_lines = [line for line in text.split('\n') if any(char.isdigit() or re.search('[零壱弐参伍拾一二三四五六七八九十百千万億兆]', char) for char in line)]
+    return number_lines
+
+def filter_alphabets(text):
+    alphabet_characters = set("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ")
+    alphabet_lines = [line for line in text.split('\n') if any(char in alphabet_characters for char in line)]
+    return alphabet_lines
+
 def filter_and_download(text, filter_type):
     if filter_type == 'katakana':
         filtered_lines = filter_katakana(text)
@@ -28,3 +59,32 @@ def filter_and_download(text, filter_type):
         st.download_button(label="テキストファイルとしてダウンロード", data=result_text, file_name=file_name, key=f"{filter_type}_download_button")
     else:
         st.write(f"テキストに対象の行は見つかりませんでした。")
+
+def main():
+    st.title("文章フィルター")
+
+    uploaded_file = st.file_uploader("テキストファイルまたはワードファイルをアップロードしてください", type=["txt", "docx"])
+
+    if uploaded_file is not None:
+        if uploaded_file.type == "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+            text = read_word_file(uploaded_file)
+        else:
+            text = uploaded_file.read().decode("utf-8")
+
+        word_counts = count_words(text)
+        result_df = pd.DataFrame(list(word_counts.items()), columns=['単語', '出現回数'])
+        st.write("### 原本")
+        st.dataframe(result_df)
+
+        if st.button("カタカナを含む文を抽出"):
+            filter_and_download(text, 'katakana')
+
+        if st.button("数字（漢数字を含む）を含む文を抽出"):
+            filter_and_download(text, 'numbers')
+
+        if st.button("アルファベットを含む文を抽出"):
+            filter_and_download(text, 'alphabets')
+
+if __name__ == "__main__":
+    main()
+
