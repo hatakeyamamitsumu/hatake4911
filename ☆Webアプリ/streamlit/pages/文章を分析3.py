650c0e3bdc1322e3a71f564cca6201e9a6225d0c
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-02-23 23:57:02 +0900

    Update 文章を分析3.py

--- ☆Webアプリ/streamlit/pages/文章を分析3.py
+++ ☆Webアプリ/streamlit/pages/文章を分析3.py
@@ -16,14 +16,35 @@ def read_word_file(file):
         text += paragraph.text + "\n"
     return text
 
-def filter_keywords(text, keywords):
-    keyword_lines = [line for line in text.split('\n') if any(keyword in line for keyword in keywords)]
-    return keyword_lines
+def filter_all_katakana(text):
+    all_katakana_lines = [line for line in text.split('\n') if all('\u30A1' <= char <= '\u30F6' for char in line)]
+    return all_katakana_lines
 
-def filter_and_download(text, filter_type, keywords):
-    if filter_type == 'custom':
-        filtered_lines = filter_keywords(text, keywords)
-        result_label = "### カスタム条件を満たす行のリスト"
+def filter_all_alphabets(text):
+    all_alphabet_characters = set("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ")
+    all_alphabet_lines = [line for line in text.split('\n') if all(char in all_alphabet_characters for char in line)]
+    return all_alphabet_lines
+
+def filter_and_download(text, filter_type):
+    if filter_type == 'all_katakana':
+        filtered_lines = filter_all_katakana(text)
+        result_label = "### 全てのカタカナを含む行のリスト"
+        file_extension = "txt"
+    elif filter_type == 'all_alphabets':
+        filtered_lines = filter_all_alphabets(text)
+        result_label = "### 全てのアルファベットを含む行のリスト"
+        file_extension = "txt"
+    elif filter_type == 'katakana_or_alphabets':
+        katakana_lines = filter_all_katakana(text)
+        alphabet_lines = filter_all_alphabets(text)
+        filtered_lines = katakana_lines + alphabet_lines
+        result_label = "### 全てのカタカナ or 全てのアルファベットを含む行のリスト"
+        file_extension = "txt"
+    elif filter_type == 'katakana_and_alphabets':
+        katakana_lines = filter_all_katakana(text)
+        alphabet_lines = filter_all_alphabets(text)
+        filtered_lines = list(set(katakana_lines) & set(alphabet_lines))
+        result_label = "### 全てのカタカナ and 全てのアルファベットを含む行のリスト"
         file_extension = "txt"
     else:
         st.error("無効なフィルタータイプが選択されました。")
@@ -59,10 +80,17 @@ def main():
         st.write("### 原本")
         st.dataframe(result_df)
 
-        keywords = ['すべてのカタカナ', 'すべてのアルファベット', 'すべての数字']
+        if st.button("全てのカタカナを含む文を抽出"):
+            filter_and_download(text, 'all_katakana')
 
-        if st.button("カスタム条件を満たす文を抽出"):
-            filter_and_download(text, 'custom', keywords)
+        if st.button("全てのアルファベットを含む文を抽出"):
+            filter_and_download(text, 'all_alphabets')
+
+        if st.button("全てのカタカナ or 全てのアルファベットを含む文を抽出"):
+            filter_and_download(text, 'katakana_or_alphabets')
+
+        if st.button("全てのカタカナ and 全てのアルファベットを含む文を抽出"):
+            filter_and_download(text, 'katakana_and_alphabets')
 
 if __name__ == "__main__":
     main()
