fc54958f0ff4c0d6d9ea49315056e6b69418d44e
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-02-24 21:42:41 +0900

    Update 動画をGIFに.py

--- ☆Webアプリ/streamlit/pages/動画をGIFに.py
+++ ☆Webアプリ/streamlit/pages/動画をGIFに.py
@@ -1,10 +1,7 @@
 import streamlit as st
 from moviepy.editor import VideoFileClip
-import base64
 
-def convert_to_gif(input_video, output_gif, fps=10, resize_percentage=None):
-    clip = VideoFileClip(input_video)
-
+def convert_to_gif(clip, output_gif, fps=10, resize_percentage=None):
     # 動画サイズ変更
     if resize_percentage:
         resize_factor = resize_percentage / 100.0
@@ -18,20 +15,23 @@ def main():
     # 動画ファイルのアップロード
     uploaded_file = st.file_uploader("動画ファイルをアップロードしてください", type=["mp4", "avi", "mov"])
 
+    # FPSの入力
+    fps = st.slider("変換するGIFのフレームレートを選択してください", min_value=1, max_value=30, value=10)
+
+    # サイズ変更のスライダー
+    resize_percentage = st.slider("動画のサイズを変更する（元の大きさの何％か）", min_value=1, max_value=100, value=100)
+
     if uploaded_file is not None:
-        # 出力GIFファイル名の入力
-        output_gif_name = st.text_input("出力GIFファイル名を入力してください", "output_animation.gif")
-
-        # FPSの入力
-        fps = st.slider("変換するGIFのフレームレートを選択してください", min_value=1, max_value=30, value=10)
-
-        # サイズ変更のスライダー
-        resize_percentage = st.slider("動画のサイズを変更する（元の大きさの何％か）", min_value=1, max_value=100, value=100)
-
+        # 動画情報取得
+        clip = VideoFileClip(uploaded_file)
+        
         # 変換ボタン
         if st.button("動画をGIFに変換"):
+            # 出力GIFファイル名の入力
+            output_gif_name = st.text_input("出力GIFファイル名を入力してください", "output_animation.gif")
+
             # 変換処理
-            convert_to_gif(uploaded_file, output_gif_name, fps, resize_percentage)
+            convert_to_gif(clip, output_gif_name, fps, resize_percentage)
             st.success("変換が完了しました！")
 
             # 変換後のGIFを表示
