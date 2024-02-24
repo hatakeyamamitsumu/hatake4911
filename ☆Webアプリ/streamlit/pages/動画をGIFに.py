df4040eae756f05b975b0fb460a759c4d51e1ffb
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-02-24 21:16:41 +0900

    Update 動画をGIFに.py

--- ☆Webアプリ/streamlit/pages/動画をGIFに.py
+++ ☆Webアプリ/streamlit/pages/動画をGIFに.py
@@ -1,14 +1,18 @@
 import streamlit as st
 from moviepy.editor import VideoFileClip
 
-def convert_video_to_gif(input_file, output_file, fps=10, target_size=None):
-    clip = VideoFileClip(input_file)
+def convert_video_to_gif(input_file, output_file, fps=10, resize_percentage=None):
+    if input_file.name.endswith(('.mp4', '.avi', '.mov')):
+        clip = VideoFileClip(input_file)
 
-    # 動画サイズ変更
-    if target_size:
-        clip = clip.resize(target_size)
+        # 動画サイズ変更
+        if resize_percentage:
+            resize_factor = resize_percentage / 100.0
+            clip = clip.resize(resize_factor)
 
-    clip.write_gif(output_file, fps=fps)
+        clip.write_gif(output_file, fps=fps)
+    else:
+        st.error("サポートされていない動画ファイル形式です。対応する形式をアップロードしてください。")
 
 def main():
     st.title("動画をGIFに変換するアプリ")
@@ -23,19 +27,13 @@ def main():
         # FPSの入力
         fps = st.slider("変換するGIFのフレームレートを選択してください", min_value=1, max_value=30, value=10)
 
-        # 動画サイズ変更の設定
-        resize_option = st.checkbox("動画のサイズを変更する", value=False)
-        if resize_option:
-            width = st.number_input("変更後の幅を入力してください", min_value=1)
-            height = st.number_input("変更後の高さを入力してください", min_value=1)
-            target_size = (width, height)
-        else:
-            target_size = None
+        # サイズ変更のスライダー
+        resize_percentage = st.slider("動画のサイズを変更する（元の大きさの何％か）", min_value=1, max_value=100, value=100)
 
         # 変換ボタン
         if st.button("動画をGIFに変換"):
             # 変換処理
-            convert_video_to_gif(uploaded_file, output_gif_name, fps, target_size)
+            convert_video_to_gif(uploaded_file, output_gif_name, fps, resize_percentage)
             st.success("変換が完了しました！")
 
             # 変換後のGIFを表示
@@ -43,3 +41,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
