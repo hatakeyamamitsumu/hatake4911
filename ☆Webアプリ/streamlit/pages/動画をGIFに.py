fe725a3b884ff7a113d7b5fd9d9f199a4fc7449a
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-02-24 21:40:31 +0900

    Update 動画をGIFに.py

--- ☆Webアプリ/streamlit/pages/動画をGIFに.py
+++ ☆Webアプリ/streamlit/pages/動画をGIFに.py
@@ -1,29 +1,46 @@
 import streamlit as st
-import cv2
-from PIL import Image
+from moviepy.editor import VideoFileClip
+import base64
 
-uploaded_video = st.file_uploader("Choose video", type=["mp4", "mov"])
-frame_skip = 300  # 300フレームごとに表示
+def convert_to_gif(input_video, output_gif, fps=10, resize_percentage=None):
+    clip = VideoFileClip(input_video)
 
-if uploaded_video is not None:
-    # ユーザーがビデオをアップロードした場合のみ実行
-    vid = uploaded_video.name
-    with open(vid, mode="wb") as f:
-        f.write(uploaded_video.read())  # ビデオをディスクに保存
+    # 動画サイズ変更
+    if resize_percentage:
+        resize_factor = resize_percentage / 100.0
+        clip = clip.resize(resize_factor)
 
-    st.markdown(f"### Files - {vid}", unsafe_allow_html=True)  # ファイル名を表示
+    clip.write_gif(output_gif, fps=fps)
 
-    vidcap = cv2.VideoCapture(vid)  # ディスクからビデオを読み込む
-    cur_frame = 0
-    success = True
+def main():
+    st.title("動画をGIFに変換するアプリ")
 
-    while success:
-        success, frame = vidcap.read()  # 次のフレームを取得
-        if cur_frame % frame_skip == 0:
-            # 300フレームごとにのみ分析
-            pil_img = Image.fromarray(frame)  # OpenCVフレームをPILイメージに変換
-            st.image(pil_img)  # 画像を表示
-        cur_frame += 1
+    # 動画ファイルのアップロード
+    uploaded_file = st.file_uploader("動画ファイルをアップロードしてください", type=["mp4", "avi", "mov"])
 
-    # ダウンロードボタンを追加
-    st.markdown(f'<a href="{vid}" download>Download GIF</a>', unsafe_allow_html=True)
+    if uploaded_file is not None:
+        # 出力GIFファイル名の入力
+        output_gif_name = st.text_input("出力GIFファイル名を入力してください", "output_animation.gif")
+
+        # FPSの入力
+        fps = st.slider("変換するGIFのフレームレートを選択してください", min_value=1, max_value=30, value=10)
+
+        # サイズ変更のスライダー
+        resize_percentage = st.slider("動画のサイズを変更する（元の大きさの何％か）", min_value=1, max_value=100, value=100)
+
+        # 変換ボタン
+        if st.button("動画をGIFに変換"):
+            # 変換処理
+            convert_to_gif(uploaded_file, output_gif_name, fps, resize_percentage)
+            st.success("変換が完了しました！")
+
+            # 変換後のGIFを表示
+            st.image(output_gif_name, use_column_width=True)
+
+            # ダウンロードボタン
+            download_button_str = f"ダウンロード {output_gif_name}"
+            download_button_id = f"download_button_{output_gif_name}"
+            st.download_button(label=download_button_str, key=download_button_id, data=uploaded_file, file_name=output_gif_name)
+
+if __name__ == "__main__":
+    main()
