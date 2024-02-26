b1b56ce6574ef4d81ed8da3e449cafb89a512d1a
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-02-26 10:09:06 +0900

    Update おすすめリンク.py

--- ☆Webアプリ/streamlit/pages/おすすめリンク.py
+++ ☆Webアプリ/streamlit/pages/おすすめリンク.py
@@ -36,3 +36,11 @@ link_str = "(https://1drv.ms/f/c/25c3642a3103cdcb/EleQi7m0oTtBijUzs5uWIJsB37xylt
 # Markdownを表示。
 st.markdown(link_str, unsafe_allow_html=True)
 st.text('↑いろんなエクセルファイルが入ったフォルダです。')
+st.text('')
+
+# ハイパーリンクを表示するMarkdown文字列
+link_str = "(https://forms.gle/DUHCTT5CfajGjoGMA)"
+# Markdownを表示。
+st.markdown(link_str, unsafe_allow_html=True)
+st.text('↑アンケートにお答えください。')
+
