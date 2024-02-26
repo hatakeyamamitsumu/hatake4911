6b10a6ee99929ef917697c296bc4ae75f9cc0f9b
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-02-26 11:19:53 +0900

    Delete ☆Webアプリ/streamlit/pages/アンケート.py

--- ☆Webアプリ/streamlit/pages/アンケート.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import webbrowser
-
-def main():
-    # ページが開かれたら指定のURLにリダイレクト
-    webbrowser.open("https://forms.gle/DUHCTT5CfajGjoGMA")
-
-if __name__ == "__main__":
-    main()
