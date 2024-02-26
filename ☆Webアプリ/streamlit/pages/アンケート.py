75daac946032a810bb6ad10b3aa0d1bd97865ec6
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-02-26 11:18:06 +0900

    Create アンケート.py

---
+++ ☆Webアプリ/streamlit/pages/アンケート.py
@@ -0,0 +1,9 @@
+import streamlit as st
+import webbrowser
+
+def main():
+    # ページが開かれたら指定のURLにリダイレクト
+    webbrowser.open("https://forms.gle/DUHCTT5CfajGjoGMA")
+
+if __name__ == "__main__":
+    main()
