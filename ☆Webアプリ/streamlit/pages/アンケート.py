34452b6d8505a9d93c82461bd6a53ff69e9371e9
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-02-28 00:15:02 +0900

    Update アンケート.py

--- ☆Webアプリ/streamlit/pages/アンケート.py
+++ ☆Webアプリ/streamlit/pages/アンケート.py
@@ -4,7 +4,7 @@ link_str = "(https://forms.gle/DUHCTT5CfajGjoGMA)"
 # Markdownを表示。
 st.markdown(link_str, unsafe_allow_html=True)
 st.text('↑アンケートにお答えください。')
-QR_path='/mount/src/hatake4911/☆Webアプリ/画像/QRコード/アンケート用グーグルフォームのQRコード.png'
+QR_path='/mount/src/hatake4911/☆Webアプリ/QRコード各種/アンケートフォーム用QR.png'
 st.image(QR_path)
 
 
