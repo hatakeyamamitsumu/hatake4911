5a3504fb14ab64e72d06324922a65f7459fbcf42
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-02-28 04:30:01 +0900

    Update 文章を分析１－１.py

--- ☆Webアプリ/streamlit/pages/文章を分析１－１.py
+++ ☆Webアプリ/streamlit/pages/文章を分析１－１.py
@@ -13,29 +13,16 @@ def read_additional_words_from_csv(file_path):
     additional_words = additional_words_df['単語'].tolist()
     return additional_words
 
-def filter_and_download(text, filter_words, additional_words=[]):
+def filter_and_display(text, filter_words, additional_words=[]):
     lines_with_words = [line for line in text.split('\n') if any(word.lower() in line.lower() for word in filter_words)]
 
     if additional_words:
         lines_with_words = [line for line in lines_with_words if any(add_word.lower() in line.lower() for add_word in additional_words)]
 
     if lines_with_words:
-        # Extracting lines with only filter_words
-        filtered_lines = [line for line in lines_with_words if all(word.lower() in line.lower() for word in filter_words)]
-
-        if filtered_lines:
-            result_df = pd.DataFrame({"行": filtered_lines})
-            st.write(f"### '{', '.join(filter_words)}' のみを含む行のリスト (or条件)")
-            st.dataframe(result_df)
-
-            result_text = "\n".join(filtered_lines)
-            file_name = "filtered_data.txt"
-            with open(file_name, 'w', encoding='utf-8') as file:
-                file.write(result_text)
-
-            st.download_button(label="テキストファイルとしてダウンロード", data=result_text, file_name=file_name, key="download_button")
-        else:
-            st.write(f"テキストに '{', '.join(filter_words)}' のみを含む行は見つかりませんでした。")
+        result_df = pd.DataFrame({"行": lines_with_words})
+        st.write(f"### '{', '.join(filter_words + additional_words)}' を含む行のリスト (or 条件)")
+        st.dataframe(result_df)
     else:
         st.write(f"テキストに '{', '.join(filter_words + additional_words)}' を含む行は見つかりませんでした。")
 
@@ -53,10 +40,11 @@ def main():
         additional_words_file_path = "/mount/src/hatake4911/☆Webアプリ/CSVファイル各種/文章分析用CSV/自然.csv"
         additional_words = read_additional_words_from_csv(additional_words_file_path)
 
-        filter_words = additional_words  # 使用する単語を additional_words に設定
+        filter_condition = 'or'  # 'or' 条件に変更
 
-        if filter_words:
-            filter_and_download(text, filter_words, additional_words)
+        if additional_words:
+            filter_and_display(text, additional_words, additional_words)
 
 if __name__ == "__main__":
     main()
+
