557e06a9c6899c623cb18c0ce15271e9e5b4cd39
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-03-03 10:14:08 +0900

    Update 文章を分析(キーワード入力).py

--- ☆Webアプリ/streamlit/pages/文章を分析(キーワード入力).py
+++ ☆Webアプリ/streamlit/pages/文章を分析(キーワード入力).py
@@ -48,6 +48,7 @@ def filter_and_download(text, filter_words, filter_condition):
 
 def main():
     st.title("文章フィルター")
+    st.text("作者があらかじめ集めた単語群が含まれる行を抽出するアプリです。特定の文脈の内容をざっくりと抽出するときにご利用ください。")
 
     # File upload
     uploaded_file = st.file_uploader("テキストファイルまたはワードファイルをアップロードしてください", type=["txt", "docx"])
