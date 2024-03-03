4fd7c26bd9a55e18e2db5a6b49098683f4fbd950
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-03-03 13:39:27 +0900

    Update main_app.py

--- ☆Webアプリ/streamlit/main_app.py
+++ ☆Webアプリ/streamlit/main_app.py
@@ -4,16 +4,7 @@ import os
 import random
 
 st.title('Hat')
-st.write('こんにちは！Hatと申します。。')
-
-image_folder_path = '/mount/src/hatake4911/☆Webアプリ/画像/東京画像'
-image_files = [f for f in os.listdir(image_folder_path) if f.lower().endswith(('png', 'jpg', 'jpeg', 'gif'))]
-selected_image = random.choice(image_files)
-selected_image_path = os.path.join(image_folder_path, selected_image)
-st.text('こちらは2022年に東京に旅行した際の写真です。ランダムに表示されます。')
-st.text(selected_image)
-st.image(selected_image_path)
-
+st.write('こんにちは！Hatと申します。')
 
 st.subheader('説明')
 st.write('プログラミングは初心者ですが、簡易なWEBアプリ「streamlit」を使って何かできないかと考えて、発表し続けようと考えております。\nよろしくお願いします。')
@@ -28,7 +19,13 @@ QR_path='/mount/src/hatake4911/☆Webアプリ/QRコード各種/アンケート
 st.image(QR_path)
 
 
-
+image_folder_path = '/mount/src/hatake4911/☆Webアプリ/画像/東京画像'
+image_files = [f for f in os.listdir(image_folder_path) if f.lower().endswith(('png', 'jpg', 'jpeg', 'gif'))]
+selected_image = random.choice(image_files)
+selected_image_path = os.path.join(image_folder_path, selected_image)
+st.text('こちらは2022年に東京に旅行した際の写真です。ランダムに表示されます。')
+st.text(selected_image)
+st.image(selected_image_path)
 
 #code = '''
 #cwd = os.getcwd()
