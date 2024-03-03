90704ac7cdd62872bb0cf33e50346e85265a9c0d
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-03-03 13:38:03 +0900

    Update main_app.py

--- ☆Webアプリ/streamlit/main_app.py
+++ ☆Webアプリ/streamlit/main_app.py
@@ -5,6 +5,16 @@ import random
 
 st.title('Hat')
 st.write('こんにちは！Hatと申します。。')
+
+image_folder_path = '/mount/src/hatake4911/☆Webアプリ/画像/東京画像'
+image_files = [f for f in os.listdir(image_folder_path) if f.lower().endswith(('png', 'jpg', 'jpeg', 'gif'))]
+selected_image = random.choice(image_files)
+selected_image_path = os.path.join(image_folder_path, selected_image)
+st.text('こちらは2022年に東京に旅行した際の写真です。ランダムに表示されます。')
+st.text(selected_image)
+st.image(selected_image_path)
+
+
 st.subheader('説明')
 st.write('プログラミングは初心者ですが、簡易なWEBアプリ「streamlit」を使って何かできないかと考えて、発表し続けようと考えております。\nよろしくお願いします。')
 st.text('オリジナルでないコードも含まれますが、可能な限り出典を明示させていただきます。')
@@ -16,25 +26,9 @@ st.markdown(link_str, unsafe_allow_html=True)
 st.text('無記名アンケートです。差し支えなければお答えください。')
 QR_path='/mount/src/hatake4911/☆Webアプリ/QRコード各種/アンケートフォーム用QR.png'
 st.image(QR_path)
-# フォルダのパス
-image_folder_path = '/mount/src/hatake4911/☆Webアプリ/画像/東京画像'
-
-# フォルダ内の画像ファイルのリストを取得
-image_files = [f for f in os.listdir(image_folder_path) if f.lower().endswith(('png', 'jpg', 'jpeg', 'gif'))]
-
-# ランダムに一つの画像を選択
-selected_image = random.choice(image_files)
-
-# 選択された画像のフルパス
-selected_image_path = os.path.join(image_folder_path, selected_image)
 
 
 
-# 選択された画像を表示
-st.text('こちらは2022年に東京に旅行した際の写真です。ランダムに表示されます。')
-# 選択された画像のファイル名を表示
-st.text(selected_image)
-st.image(selected_image_path)
 
 #code = '''
 #cwd = os.getcwd()
