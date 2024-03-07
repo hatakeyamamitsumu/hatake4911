b1a1ee6a02384145068b68300be7e9e5c0fede41
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-03-07 13:26:28 +0900

    Update おすすめリンク.py

--- ☆Webアプリ/streamlit/pages/おすすめリンク.py
+++ ☆Webアプリ/streamlit/pages/おすすめリンク.py
@@ -36,6 +36,4 @@ selected_image = st.selectbox("QRコード用意してます。リストから�
 # 選択された画像ファイルのパスを作成
 selected_image_path = os.path.join(image_folder_path, selected_image)
 
-# 選択された画像を表示
-st.image(selected_image_path, caption=f"選択された画像ファイル: {selected_image}")
 
