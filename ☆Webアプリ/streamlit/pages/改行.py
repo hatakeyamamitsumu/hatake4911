36b876934393e31c663824fa753174913a3c34e1
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-03-11 13:05:01 +0900

    Update 改行.py

--- ☆Webアプリ/streamlit/pages/改行.py
+++ ☆Webアプリ/streamlit/pages/改行.py
@@ -1,16 +1,20 @@
 import streamlit as st
 import re
 
-# アップロードされたテキストファイルを取得
-uploaded_file = st.file_uploader("テキストファイルを選択してください")
+def main():
+    # アップロードされたテキストファイルを取得
+    uploaded_file = st.file_uploader("テキストファイルを選択してください")
 
-# テキストファイルの内容を読み込み
-if uploaded_file is not None:
-    text = uploaded_file.read().decode("utf-8")
+    # テキストファイルの内容を読み込み
+    if uploaded_file is not None:
+        text = uploaded_file.read().decode("utf-8")
 
-# テキストファイルを「。」「.」「．」で分割
-split_text = re.split(r"[。.．]", text)
+    # テキストファイルを「。」「.」「．」で分割
+    split_text = re.split(r"[。.．]", text)
 
-# 分割された結果を改行して表示
-for sentence in split_text:
-    st.write(sentence + "\r\n")
+    # 分割された結果を改行して表示
+    for sentence in split_text:
+        st.write(sentence + "\r\n")
+
+if __name__ == "__main__":
+    main()
