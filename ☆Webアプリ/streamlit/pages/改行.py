1d8e5c6769aa88818e88c8e40abec118d76cc189
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-03-11 12:58:53 +0900

    Create 改行.py

---
+++ ☆Webアプリ/streamlit/pages/改行.py
@@ -0,0 +1,15 @@
+import re
+
+# アップロードされたテキストファイルを取得
+uploaded_file = st.file_uploader("テキストファイルを選択してください")
+
+# テキストファイルの内容を読み込み
+if uploaded_file is not None:
+    text = uploaded_file.read().decode("utf-8")
+
+# テキストファイルを「。」「.」「．」で分割
+split_text = re.split(r"[。.．]", text)
+
+# 分割された結果を改行して表示
+for sentence in split_text:
+    st.write(sentence + "\r\n")
