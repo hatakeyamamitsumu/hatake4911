8ec241e0dced66ed01e40ff281ae713623b9515a
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-03-11 13:08:00 +0900

    Update 改行.py

--- ☆Webアプリ/streamlit/pages/改行.py
+++ ☆Webアプリ/streamlit/pages/改行.py
@@ -9,12 +9,13 @@ def main():
     if uploaded_file is not None:
         text = uploaded_file.read().decode("utf-8")
 
-    # テキストファイルを「。」「.」「．」で分割
-    split_text = re.split(r"[。.．]", text)
+        # テキストファイルを「。」「.」「．」で分割
+        split_text = re.split(r"[。.．]", text)
 
-    # 分割された結果を改行して表示
-    for sentence in split_text:
-        st.write(sentence + "\r\n")
+        # 分割された結果を改行して表示
+        for sentence in split_text:
+            st.write(sentence + "\r\n")
 
 if __name__ == "__main__":
     main()
+
