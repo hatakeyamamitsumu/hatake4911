e19ddb32cae9b6961a6d1ae4d3c0ca2899c835da
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-03-11 11:12:00 +0900

    Update ２.py

--- ☆Webアプリ/streamlit/pages/２.py
+++ ☆Webアプリ/streamlit/pages/２.py
@@ -16,3 +16,20 @@ st.title("おすすめリンク")
 # 選択ボックス
 
 selected_index = st.selectbox("表示したいリンク番号を選択してください", range(len(links)))
+# 選択されたリンクと説明を表示
+selected_link, selected_description = links[selected_index]
+st.markdown(f"{selected_link} {selected_description}", unsafe_allow_html=True)
+
+
+# 画像ファイルが保存されているフォルダのパス
+image_folder_path = "/mount/src/hatake4911/☆Webアプリ/QRコード各種"
+
+# フォルダ内の画像ファイルのリストを取得
+image_files = [f for f in os.listdir(image_folder_path) if f.endswith(('.png', '.jpg', '.jpeg', '.gif'))]
+
+# 画像ファイルを選択するセレクトボックスを表示
+selected_image = st.selectbox("QRコード用意してます。リストから選択してください", image_files)
+
+# 選択された画像ファイルのパスを作成
+selected_image_path = os.path.join(image_folder_path, selected_image)
+st.image(selected_image_path, caption=f"選択された画像ファイル: {selected_image}")
