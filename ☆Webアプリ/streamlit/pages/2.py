b031169c894513225179be7b60ab68fd93a63375
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-03-11 11:29:59 +0900

    Update 2.py

--- ☆Webアプリ/streamlit/pages/2.py
+++ ☆Webアプリ/streamlit/pages/2.py
@@ -3,11 +3,11 @@ import streamlit as st
 
 # リンクと説明のリスト。
 links = [
-    ("https://williammer.github.io/works/shodo/", "←　簡単な書道ができるフリーソフトです。"),
-    ("https://gigafile.nu/", "←　ギガファイルサービス。"),
-    ("https://qrcode.onl.jp/", "←　QRコードリーダーです。"),
-    ("https://1drv.ms/f/c/25c3642a3103cdcb/EtW74Af8pZJEvbgsxfhCAgoBBufG7sLiDQJKDcu2UhWzNw?e=6KTvOs", "←　Hatの画像フォルダ。"),
-    ("https://1drv.ms/f/c/25c3642a3103cdcb/EleQi7m0oTtBijUzs5uWIJsB37xyltZG6PP6_LzORRJFqQ?e=Guz12t", "←　Hatのプライベートフォルダ。いろんなエクセルファイルが入ったフォルダです。"),
+    ("https://williammer.github.io/works/shodo/", "簡単な書道ができるフリーソフトです。"),
+    ("https://gigafile.nu/", "ギガファイルサービス。"),
+    ("https://qrcode.onl.jp/", "QRコードリーダーです。"),
+    ("https://1drv.ms/f/c/25c3642a3103cdcb/EtW74Af8pZJEvbgsxfhCAgoBBufG7sLiDQJKDcu2UhWzNw?e=6KTvOs", "Hatの画像フォルダ。"),
+    ("https://1drv.ms/f/c/25c3642a3103cdcb/EleQi7m0oTtBijUzs5uWIJsB37xyltZG6PP6_LzORRJFqQ?e=Guz12t", "Hatのプライベートフォルダ。いろんなエクセルファイルが入ったフォルダです。"),
 ]
 
 # タイトル
