2dae99d37bad4e9dcc398c92430b1b446a248d5b
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-03-20 23:50:12 +0900

    Update QRコードに説明写真を添える.py

--- ☆Webアプリ/streamlit/pages/QRコードに説明写真を添える.py
+++ ☆Webアプリ/streamlit/pages/QRコードに説明写真を添える.py
@@ -3,7 +3,7 @@ import qrcode
 from PIL import Image, ImageDraw, ImageFont
 import io
 
-st.title('QRコードに背景をつけて、分かりやすく！')ツール
+st.title('QRコードに背景をつけて、分かりやすく！')
 st.write('何のQRコードか分かりやすくなります。')
 def generate_qr_code(data, size=500):
     qr_img = qrcode.make(data)
