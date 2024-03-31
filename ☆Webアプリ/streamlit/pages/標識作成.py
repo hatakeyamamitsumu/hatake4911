c83a3b78faf0d89a36126957c35d91dc336830aa
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-04-01 06:45:43 +0900

    Update 標識作成.py

--- ☆Webアプリ/streamlit/pages/標識作成.py
+++ ☆Webアプリ/streamlit/pages/標識作成.py
@@ -11,6 +11,7 @@ def main():
         "/mount/src/hatake4911/☆Webアプリ/画像/標識用画像/第二層",
         "/mount/src/hatake4911/☆Webアプリ/画像/標識用画像/第三層",
         "/mount/src/hatake4911/☆Webアプリ/画像/標識用画像/第四層",
+        "/mount/src/hatake4911/☆Webアプリ/画像/標識用画像/第五層",
     ]
 
     # 画像ファイルの選択
