5bcb5c552c71f8951ad2436df5665d7f71b82823
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-04-12 21:37:45 +0900

    Update 標識作成3.py

--- ☆Webアプリ/streamlit/pages/標識作成3.py
+++ ☆Webアプリ/streamlit/pages/標識作成3.py
@@ -27,26 +27,33 @@ def main():
     # 画像リストの初期化
     uploaded_images = []
 
-    # 下に重ねる画像がアップロードされた場合
-    if uploaded_image_bottom is not None:
-        ImgObj_bottom = Image.open(uploaded_image_bottom)
-        ImgObj_bottom = ImgObj_bottom.convert('RGBA') if ImgObj_bottom.mode == "RGB" else ImgObj_bottom  # JPEGをRGBAに変換
-        uploaded_images.append(center_align(ImgObj_bottom))  # 修正点: 一番下に追加する
-
     # 上に重ねる画像がアップロードされた場合
     if uploaded_image_top is not None:
         ImgObj_top = Image.open(uploaded_image_top)
         ImgObj_top = ImgObj_top.convert('RGBA') if ImgObj_top.mode == "RGB" else ImgObj_top  # JPEGをRGBAに変換
         # Remove background from the uploaded image
         ImgObj_top = remove_background(ImgObj_top)
-        uploaded_images.insert(0, center_align(ImgObj_top))  # 修正点: 一番上に追加する
+        uploaded_images.append(center_align(ImgObj_top))  # 修正点: 一番上に追加する
 
     # 画像ファイルの選択（第四層以外）
     for folder in image_folders:
-        if not folder.endswith("第四層"):
+        if not folder.endswith("第四層") and not folder.endswith("第六層"):  # 第四層と第六層を除外
             image_files = os.listdir(folder)
             selected_image = st.selectbox("", image_files, index=0)
             uploaded_images.append(center_align(Image.open(os.path.join(folder, selected_image))))
+
+    # 第六層の画像を追加
+    for folder in image_folders:
+        if folder.endswith("第六層"):  # 第六層の画像を追加
+            image_files = os.listdir(folder)
+            selected_image = st.selectbox("", image_files, index=0)
+            uploaded_images.append(center_align(Image.open(os.path.join(folder, selected_image))))
+
+    # 下に重ねる画像がアップロードされた場合
+    if uploaded_image_bottom is not None:
+        ImgObj_bottom = Image.open(uploaded_image_bottom)
+        ImgObj_bottom = ImgObj_bottom.convert('RGBA') if ImgObj_bottom.mode == "RGB" else ImgObj_bottom  # JPEGをRGBAに変換
+        uploaded_images.append(center_align(ImgObj_bottom))  # 修正点: 一番下に追加する
 
     # 他の画像のサイズに合わせて縮小拡大
     max_width = max(img.size[0] for img in uploaded_images)
@@ -113,5 +120,3 @@ def remove_background(image):
 
 if __name__ == '__main__':
     main()
-
-  
