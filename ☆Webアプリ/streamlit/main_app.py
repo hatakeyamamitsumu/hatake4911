693b60f88fd5cd7855ce95846608a2193e1decbb
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-04-24 18:23:39 +0900

    Update main_app.py

--- ☆Webアプリ/streamlit/main_app.py
+++ ☆Webアプリ/streamlit/main_app.py
@@ -2,8 +2,7 @@ import streamlit as st
 from PIL import Image
 import os
 import random
-import io
-import qrcode
+
 
 st.title('Hatの、WEBアプリ作り挑戦日記')
 st.write('こんにちは！Hatと申します。')
@@ -32,40 +31,3 @@ st.text(selected_image)
 st.image(selected_image_path)
 
 
-# リンクと説明のリスト
-links = [
-    ("簡単な書道ができるフリーソフトです。", "https://williammer.github.io/works/shodo/"),
-    ("ギガファイルサービス。", "https://gigafile.nu/"),
-    ("QRコードリーダーです。", "https://qrcode.onl.jp/"),
-    ("Hatの画像フォルダ。", "https://1drv.ms/f/c/25c3642a3103cdcb/EtW74Af8pZJEvbgsxfhCAgoBBufG7sLiDQJKDcu2UhWzNw?e=6KTvOs"),
-    ("Hatのプライベートフォルダ。いろんなエクセルファイルが入ったフォルダです。", "https://1drv.ms/f/c/25c3642a3103cdcb/EleQi7m0oTtBijUzs5uWIJsB37xyltZG6PP6_LzORRJFqQ?e=Guz12t"),
-]
-
-# タイトル
-st.title("おすすめリンク")
-
-# 選択ボックス
-selected_link = st.selectbox("表示したいリンクを選択してください", links)
-
-# 選択されたリンクと説明を表示
-st.markdown(f"""**リンク:** {selected_link[1]}""", unsafe_allow_html=True)
-
-# 選択されたリンクのQRコードを生成してバイトデータとして取得
-qr = qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_L, box_size=10, border=4)
-qr.add_data(selected_link[1])
-qr.make(fit=True)
-img = qr.make_image(fill_color="black", back_color="white")
-
-# PILイメージをバイトデータに変換
-img_byte_arr = io.BytesIO()
-img.save(img_byte_arr, format='PNG')
-img_byte_arr = img_byte_arr.getvalue()
-
-# Streamlitにバイトデータを表示　, use_column_width=True
-st.image(img_byte_arr, caption="QRコード")
-
-#code = '''
-#cwd = os.getcwd()
-#st.text(cwd)  # このコードによってgithub上のフルパスを確認
-#'''
-#st.code(code, language='python')
