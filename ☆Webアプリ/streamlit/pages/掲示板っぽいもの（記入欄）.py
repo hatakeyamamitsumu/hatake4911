cb1230fd4a02b623aa04b3cf0de617c3397c79ca
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-04-25 12:09:54 +0900

    Update 掲示板っぽいもの（記入欄）.py

--- ☆Webアプリ/streamlit/pages/掲示板っぽいもの（記入欄）.py
+++ ☆Webアプリ/streamlit/pages/掲示板っぽいもの（記入欄）.py
@@ -10,16 +10,17 @@ st.title('簡易な掲示板っぽいアプリ')
 SP_CREDENTIAL_FILE = '/mount/src/hatake4911/☆Webアプリ/秘密鍵/gspread-test-421301-6cd8b0cc0e27.json'
 SP_SCOPE = ['https://www.googleapis.com/auth/drive', 'https://spreadsheets.google.com/feeds']
 SP_SHEET_KEY = '1GuaN72pbZxQJBsxLTK3n7fAQfLMJIcuUjZ7pBd-R7kc'
+SP_SHEET_NAME = 'DEMO'
 
 # 認証情報の読み込みと認証
 credentials = ServiceAccountCredentials.from_json_keyfile_name(SP_CREDENTIAL_FILE, SP_SCOPE)
 gc = gspread.authorize(credentials)
 
-# スプレッドシートの選択
-selected_sheet = st.selectbox("シートを選択してください:", [worksheet.title for worksheet in gc.open_by_key(SP_SHEET_KEY).worksheets()])
+# スプレッドシートの指定
+sh = gc.open_by_key(SP_SHEET_KEY)
+worksheet = sh.worksheet(SP_SHEET_NAME)
 
-# 選択されたシートのデータ取得
-worksheet = gc.open_by_key(SP_SHEET_KEY).worksheet(selected_sheet)
+# 現在のデータ取得
 data = worksheet.get_all_values()
 df = pd.DataFrame(data[1:], columns=data[0])
 
