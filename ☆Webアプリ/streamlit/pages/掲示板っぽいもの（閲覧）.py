a312f1d855490e5813e8377368190006d0243836
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-04-25 12:28:50 +0900

    Update 掲示板っぽいもの（閲覧）.py

--- ☆Webアプリ/streamlit/pages/掲示板っぽいもの（閲覧）.py
+++ ☆Webアプリ/streamlit/pages/掲示板っぽいもの（閲覧）.py
@@ -2,8 +2,8 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import json
 import pandas as pd
-
-
+import streamlit as st
+st.title('簡易な掲示板っぽいアプリ（閲覧コーナー）')
 SP_CREDENTIAL_FILE = '/mount/src/hatake4911/☆Webアプリ/秘密鍵/gspread-test-421301-6cd8b0cc0e27.json'
 
 SP_COPE = [
@@ -18,7 +18,7 @@ sh=gc.open_by_key(SP_SHEET_KEY)
 worksheet=sh.worksheet(SP_SHEET)
 data=worksheet.get_all_values()
 #data
-st.title('簡易な掲示板っぽいアプリ（閲覧コーナー）')
+
 df=pd.DataFrame(data[1:],columns=data[0])
 df
 
