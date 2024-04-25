33740ed8eb1f0ad7845022488fd6e5d760374f99
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-04-25 11:56:39 +0900

    Update 掲示板っぽいもの（閲覧）.py

--- ☆Webアプリ/streamlit/pages/掲示板っぽいもの（閲覧）.py
+++ ☆Webアプリ/streamlit/pages/掲示板っぽいもの（閲覧）.py
@@ -2,21 +2,29 @@ import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 import json
 import pandas as pd
+from ipywidgets import interact, widgets
+
 SP_CREDENTIAL_FILE = '/mount/src/hatake4911/☆Webアプリ/秘密鍵/gspread-test-421301-6cd8b0cc0e27.json'
 
-SP_COPE = [
+SP_SCOPE = [
     'https://www.googleapis.com/auth/drive',
     'https://spreadsheets.google.com/feeds'
 ]
 SP_SHEET_KEY = '1GuaN72pbZxQJBsxLTK3n7fAQfLMJIcuUjZ7pBd-R7kc'
-SP_SHEET='話題1'
-credentials =ServiceAccountCredentials.from_json_keyfile_name(SP_CREDENTIAL_FILE,SP_COPE)
-gc=gspread.authorize(credentials)
-sh=gc.open_by_key(SP_SHEET_KEY)
-worksheet=sh.worksheet(SP_SHEET)
-data=worksheet.get_all_values()
-#data
-df=pd.DataFrame(data[1:],columns=data[0])
-df
 
-#df.sort_values('社員ID')
+# Google Sheetsからシート名を取得する関数
+def get_sheet_names(credentials, sheet_key):
+    gc = gspread.authorize(credentials)
+    sh = gc.open_by_key(sheet_key)
+    return [worksheet.title for worksheet in sh.worksheets()]
+
+# 選択式のセレクトボックスを作成する関数
+def select_sheet(sheet_names):
+    @interact(sheet_name=sheet_names)
+    def get_selected_sheet(sheet_name):
+        return sheet_name
+
+# シート名の取得とセレクトボックスの表示
+credentials = ServiceAccountCredentials.from_json_keyfile_name(SP_CREDENTIAL_FILE, SP_SCOPE)
+sheet_names = get_sheet_names(credentials, SP_SHEET_KEY)
+select_sheet(sheet_names)
