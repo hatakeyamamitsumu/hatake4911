491e95d05767d089223f1dcab26006827d438cc0
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-04-27 03:49:37 +0900

    Update 画像取得.py

--- ☆Webアプリ/streamlit/pages/画像取得.py
+++ ☆Webアプリ/streamlit/pages/画像取得.py
@@ -35,8 +35,8 @@ def main():
     st.title('Googleドライブ内の画像を表示する')
 
     # フォルダIDの入力
-    folder_id = st.text_input('Googleドライブ内のフォルダIDを入力してください')
-
+    #folder_id = st.text_input('Googleドライブ内のフォルダIDを入力してください')
+    folder_id = '1BIEdWNQ1Iw0nEqf8OpGZXDywXFBiQueN'
     if folder_id:
         try:
             images = get_images_from_folder(folder_id)
