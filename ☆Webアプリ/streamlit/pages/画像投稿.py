73538c1f955a385f41df076ff99edf9789b02417
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-04-28 21:34:21 +0900

    Update 画像投稿.py

--- ☆Webアプリ/streamlit/pages/画像投稿.py
+++ ☆Webアプリ/streamlit/pages/画像投稿.py
@@ -8,7 +8,7 @@ import io
 #フォルダの
 # Google ドライブ API 認証情報
 credentials = Credentials.from_service_account_file(
-    '/mount/src/hatake4911/☆Webアプリ/その他/gspread-test-421301-6cd8b0cc0e27.json',
+    '/mount/src/hatake4911/☆Webアプリ/その他/gspread-test-421301-6cd8b0cc0e27.json,
     scopes=['https://www.googleapis.com/auth/drive']
 )
 
