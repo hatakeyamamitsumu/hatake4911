da02b5d304ecff4ea61833d07c2a08db61b4341c
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-04-28 22:04:36 +0900

    Update 画像投稿・検索.py

--- ☆Webアプリ/streamlit/pages/画像投稿・検索.py
+++ ☆Webアプリ/streamlit/pages/画像投稿・検索.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from google.oauth2.service_account import Credentials
 from googleapiclient.discovery import build
-from googleapiclient.http import MediaIoBaseDownload
+from googleapiclient.http import MediaIoBaseDownload, MediaIoBaseUpload
 from PIL import Image
 import io
 
