d6111e06db73d827b83846f25f8baca8d616f21b
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-05-01 09:13:14 +0900

    Update 掲示板.py

--- ☆Webアプリ/streamlit/pages/掲示板.py
+++ ☆Webアプリ/streamlit/pages/掲示板.py
@@ -4,6 +4,8 @@ import json
 import pandas as pd
 import streamlit as st
 
+
+
 # Streamlitアプリケーションのタイトルを設定
 st.title('簡易な掲示板っぽいアプリ（閲覧コーナー）')
 
@@ -41,31 +43,21 @@ df
 #df.sort_values('社員ID')
 
 
-import gspread
-from oauth2client.service_account import ServiceAccountCredentials
-import pandas as pd
-import streamlit as st
+
 
 # Streamlitアプリケーションのタイトルを設定
 st.title('簡易な掲示板っぽいアプリ（記入コーナー）')
 
-# Google Sheets 認証情報のファイルパスとスコープ
-SP_CREDENTIAL_FILE = '/mount/src/hatake4911/☆Webアプリ/その他/gspread-test-421301-6cd8b0cc0e27.json'
-SP_SCOPE = ['https://www.googleapis.com/auth/drive', 'https://spreadsheets.google.com/feeds']
-SP_SHEET_KEY = '1GuaN72pbZxQJBsxLTK3n7fAQfLMJIcuUjZ7pBd-R7kc'
 
-# 認証情報の読み込みと認証
-credentials = ServiceAccountCredentials.from_json_keyfile_name(SP_CREDENTIAL_FILE, SP_SCOPE)
-gc = gspread.authorize(credentials)
 
 # スプレッドシートの指定
 sh = gc.open_by_key(SP_SHEET_KEY)
 
 # スプレッドシート内のシート名を取得
-sheet_names = [worksheet.title for worksheet in sh.worksheets()]
+sheet_names2 = [worksheet.title for worksheet in sh.worksheets()]
 
 # セレクトボックスでシートを選択
-selected_sheet_name = st.selectbox("読みたいテーマを選択してください", sheet_names)
+selected_sheet_name = st.selectbox("読みたいテーマを選択してください", sheet_names2)
 
 # 選択したシートを開く
 worksheet = sh.worksheet(selected_sheet_name)
