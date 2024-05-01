a18facb5f1760f4bcbb9df55193d38618ffef60e
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-05-01 11:45:46 +0900

    Update 緯度経度を取得.py

--- ☆Webアプリ/streamlit/pages/緯度経度を取得.py
+++ ☆Webアプリ/streamlit/pages/緯度経度を取得.py
@@ -10,37 +10,15 @@ m = folium.Map(location=map_center, zoom_start=12)
 # 地図上のクリックしたポイントの緯度経度を格納するリスト
 clicked_points = []
 
-# 地図上のクリックを検知するJavaScriptを生成
-js_code = """
-<script>
-var map = document.getElementsByClassName('folium-map')[0];
+# 地図上のクリックを検知するコールバック関数
+def handle_click(event, **kwargs):
+    if event.originalEvent:
+        lat, lon = event.latlng
+        clicked_points.append((lat, lon))
+        st.info(f"クリックしたポイントの緯度経度: 緯度 {lat}, 経度 {lon}")
 
-// 地図上でクリックされたときの処理
-map.on('click', function(event){
-    var lat = event.latlng.lat.toFixed(6);
-    var lng = event.latlng.lng.toFixed(6);
-    var point = [lat, lng];
-    // Streamlitにクリックしたポイントを送信
-    Shiny.setInputValue('clicked_point', point);
-});
-</script>
-"""
+# 地図にクリックイベントを追加
+m.add_child(folium.ClickForMarker(popup=None, callback=handle_click))
 
-# Streamlitで地図を表示
-st.markdown(folium.Map()._repr_html_(), unsafe_allow_html=True)
-st.markdown(js_code, unsafe_allow_html=True)
-
-# Streamlitでクリックしたポイントの緯度経度を受信して表示
-clicked_point = st.empty()
-clicked_point_info = st.empty()
-
-# Streamlitでクリックしたポイントを受信して表示
-clicked_point = st.empty()
-clicked_point_info = st.empty()
-
-while True:
-    # Streamlitからクリックしたポイントを受信
-    point = st.session_state.get('clicked_point')
-    if point:
-        clicked_points.append(point)
-        clicked_point_info.write(f"クリックしたポイントの緯度経度: {point}")
+# Streamlit で地図を表示
+st.write(m)
