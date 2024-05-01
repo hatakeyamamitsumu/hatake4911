30aba22178d4acc9e968d2b0024b52ad4857ff24
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-05-01 09:10:05 +0900

    Update 掲示板.py

--- ☆Webアプリ/streamlit/pages/掲示板.py
+++ ☆Webアプリ/streamlit/pages/掲示板.py
@@ -1,14 +1,18 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import json
 import pandas as pd
 import streamlit as st
 
 # Streamlitアプリケーションのタイトルを設定
-st.title('簡易な掲示板っぽいアプリ')
+st.title('簡易な掲示板っぽいアプリ（閲覧コーナー）')
 
 # Google Sheets 認証情報のファイルパスとスコープ
 SP_CREDENTIAL_FILE = '/mount/src/hatake4911/☆Webアプリ/その他/gspread-test-421301-6cd8b0cc0e27.json'
-SP_SCOPE = ['https://www.googleapis.com/auth/drive', 'https://spreadsheets.google.com/feeds']
+SP_SCOPE = [
+    'https://www.googleapis.com/auth/drive',
+    'https://spreadsheets.google.com/feeds'
+]
 SP_SHEET_KEY = '1GuaN72pbZxQJBsxLTK3n7fAQfLMJIcuUjZ7pBd-R7kc'
 
 # 認証情報の読み込みと認証
@@ -21,43 +25,17 @@ sh = gc.open_by_key(SP_SHEET_KEY)
 # スプレッドシート内のシート名を取得
 sheet_names = [worksheet.title for worksheet in sh.worksheets()]
 
-# 書き込み機能
-if '書き込み' in st.session_state:
-    # 選択したシートを開く
-    worksheet = sh.worksheet(st.session_state.selected_sheet_name)
-    
-    # 新しいデータをユーザーから受け取る
-    new_data = []
-    data = worksheet.get_all_values()
-    df = pd.DataFrame(data[1:], columns=data[0])
-    for i, column in enumerate(df.columns):
-        new_value = st.text_input("{}を入力してください: ".format(column), key=str(i))
-        new_data.append(new_value)
+# セレクトボックスでシートを選択
+selected_sheet_name = st.selectbox("読みたいテーマを選択してください", sheet_names)
 
-    # 新しいデータを追加
-    updated_df = df.append(pd.Series(new_data, index=df.columns), ignore_index=True)
+# 選択したシートを開く
+worksheet = sh.worksheet(selected_sheet_name)
 
-    # 新しいデータをスプレッドシートに書き込む
-    worksheet.update([updated_df.columns.values.tolist()] + updated_df.values.tolist())
-    st.success("新しいメッセージを書き込みました。")
-else:
-    # 読み込み機能
-    # セレクトボックスでシートを選択
-    # セレクトボックスでシートを選択
-    selected_sheet_name = st.selectbox("読みたいテーマを選択してください", sheet_names, key='selected_sheet_name')
-    st.session_state.selected_sheet_name = selected_sheet_name
+# データ取得
+data = worksheet.get_all_values()
+df = pd.DataFrame(data[1:], columns=data[0])
 
+# データフレームを表示
+df
 
-    # 選択したシートを開く
-    worksheet = sh.worksheet(selected_sheet_name)
-
-    # データ取得
-    data = worksheet.get_all_values()
-    df = pd.DataFrame(data[1:], columns=data[0])
-
-    # データフレームを表示
-    df
-
-# メッセージを書き込むボタン
-if '書き込む' not in st.session_state:
-    st.session_state.書き込む = st.button('メッセージを書き込む（書き込んだら消せません）')
+#df.sort_values('社員ID')
