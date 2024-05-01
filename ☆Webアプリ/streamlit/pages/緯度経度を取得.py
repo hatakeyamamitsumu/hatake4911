a3df353dc4e22e850644452c2f1b25df680d0669
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-05-01 11:46:55 +0900

    Update 緯度経度を取得.py

--- ☆Webアプリ/streamlit/pages/緯度経度を取得.py
+++ ☆Webアプリ/streamlit/pages/緯度経度を取得.py
@@ -1,24 +1,13 @@
 import streamlit as st
-import folium
-
-# 地図を表示する領域を指定
-map_center = [35.6895, 139.6917]  # 東京の緯度経度
 
 # 地図を表示
-m = folium.Map(location=map_center, zoom_start=12)
+st.title('クリックしたポイントの緯度経度を取得する')
+center = [35.6895, 139.6917]  # 東京の緯度経度
+map_data = st.map(center=center, zoom=12)
 
-# 地図上のクリックしたポイントの緯度経度を格納するリスト
+# クリックしたポイントの緯度経度を表示
 clicked_points = []
 
-# 地図上のクリックを検知するコールバック関数
-def handle_click(event, **kwargs):
-    if event.originalEvent:
-        lat, lon = event.latlng
-        clicked_points.append((lat, lon))
-        st.info(f"クリックしたポイントの緯度経度: 緯度 {lat}, 経度 {lon}")
-
-# 地図にクリックイベントを追加
-m.add_child(folium.ClickForMarker(popup=None, callback=handle_click))
-
-# Streamlit で地図を表示
-st.write(m)
+if map_data:
+    clicked_points.append((map_data["lat"], map_data["lon"]))
+    st.info(f"クリックしたポイントの緯度経度: 緯度 {map_data['lat']}, 経度 {map_data['lon']}")
