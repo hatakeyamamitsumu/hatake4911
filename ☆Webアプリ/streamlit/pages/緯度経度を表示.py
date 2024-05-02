10cfc321117ab9bd812f42f4ddd7ee7ce5c76af1
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-05-03 08:22:03 +0900

    Update 緯度経度を表示.py

--- ☆Webアプリ/streamlit/pages/緯度経度を表示.py
+++ ☆Webアプリ/streamlit/pages/緯度経度を表示.py
@@ -12,19 +12,21 @@ client = gspread.authorize(creds)
 # タイトルを設定
 st.title("スプレッドシートから地図上に表示")
 
+# ユーザーにスプレッドシートを選択させる
+spreadsheet_url = st.text_input("スプレッドシートのURLを入力してください")
+
 # スプレッドシートからデータを取得
-spreadsheet_url = "https://docs.google.com/spreadsheets/d/1X1mppebuIXGIGd-n_9pL6wHahk1-rFbO2tAjgc9mEqg/edit?usp=drive_link"
-sheet = client.open_by_url(spreadsheet_url).sheet1
-data = sheet.get_all_values()
+if spreadsheet_url:
+    sheet = client.open_by_url(spreadsheet_url).sheet1
+    data = sheet.get_all_values()
 
-# 地図を作成
-m = folium.Map()
+    # 地図を作成
+    m = folium.Map()
 
-# データから緯度経度を取得し、ピンを立てる
-for row in data[1:]:  # ヘッダーを除く
-    latitude, longitude, info = float(row[0]), float(row[1]), row[2]
-    folium.Marker([latitude, longitude], popup=info).add_to(m)
+    # データから緯度経度を取得し、ピンを立てる
+    for row in data[1:]:  # ヘッダーを除く
+        latitude, longitude, info = float(row[0]), float(row[1]), row[2]
+        folium.Marker([latitude, longitude], popup=info).add_to(m)
 
-# 地図を表示
-folium_static(m)
-
+    # 地図を表示
+    folium_static(m)
