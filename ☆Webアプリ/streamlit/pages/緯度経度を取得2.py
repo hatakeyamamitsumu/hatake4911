5683d2a2aca6b8e1f5173bf95f3e401ecbc3a63b
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-05-03 07:47:02 +0900

    Update 緯度経度を取得2.py

--- ☆Webアプリ/streamlit/pages/緯度経度を取得2.py
+++ ☆Webアプリ/streamlit/pages/緯度経度を取得2.py
@@ -6,21 +6,24 @@ from streamlit_folium import folium_static
 
 # Google Sheetsの認証情報
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name("/mount/src/hatake4911/☆Webアプリ/その他/gspread-test-421301-6cd8b0cc0e27.json", scope)  # "your_credentials.json" を実際のJSONキーファイルへのパスに置き換えてください
+creds = ServiceAccountCredentials.from_json_keyfile_name("/mount/src/hatake4911/☆Webアプリ/その他/gspread-test-421301-6cd8b0cc0e27.json", scope)  
 client = gspread.authorize(creds)
 
 # タイトルを設定
-st.title("ピンを立てる")
+st.title("情報とピンを立てる")
 
 # ユーザーから緯度と経度の入力を受け取る
 latitude = st.number_input("緯度を入力してください", value=35.6895, step=0.0001)
 longitude = st.number_input("経度を入力してください", value=139.6917, step=0.0001)
 
+# ユーザーから情報の入力を受け取る
+info = st.text_input("情報を入力してください")
+
 # 地図を作成
 m = folium.Map(location=[latitude, longitude], zoom_start=10)
 
 # 入力された緯度経度にピンを立てる
-folium.Marker([latitude, longitude], popup='Selected Point').add_to(m)
+folium.Marker([latitude, longitude], popup=info).add_to(m)
 
 # 地図を表示
 folium_static(m)
@@ -32,8 +35,8 @@ if st.button("書き込み"):
     sheet = client.open_by_url(spreadsheet_url).sheet1
 
     # 新しいデータをGoogle Sheetsに書き込む
-    new_row = [latitude, longitude]
+    new_row = [latitude, longitude, info]
     sheet.append_row(new_row)
 
     # ユーザーに成功メッセージを表示
-    st.success("緯度経度がGoogle Sheetsに書き込まれました。")
+    st.success("情報と緯度経度がGoogle Sheetsに書き込まれました。")
