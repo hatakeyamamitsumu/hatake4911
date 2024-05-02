889a9ac00502e02ea4536a5fdd9457add2e7ee9f
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-05-03 07:36:06 +0900

    Update 緯度経度を取得2.py

--- ☆Webアプリ/streamlit/pages/緯度経度を取得2.py
+++ ☆Webアプリ/streamlit/pages/緯度経度を取得2.py
@@ -6,7 +6,7 @@ from streamlit_folium import folium_static
 
 # Google Sheetsの認証情報
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-creds = ServiceAccountCredentials.from_json_keyfile_name("your_credentials.json", scope)  # "your_credentials.json" を実際のJSONキーファイルへのパスに置き換えてください
+creds = ServiceAccountCredentials.from_json_keyfile_name("/mount/src/hatake4911/☆Webアプリ/その他/gspread-test-421301-6cd8b0cc0e27.json", scope)  # "your_credentials.json" を実際のJSONキーファイルへのパスに置き換えてください
 client = gspread.authorize(creds)
 
 # タイトルを設定
