db244c7087decf5d01758f72a9d6a773e713b02c
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-05-08 20:17:21 +0900

    Update 緯度経度2.py

--- ☆Webアプリ/streamlit/pages/緯度経度2.py
+++ ☆Webアプリ/streamlit/pages/緯度経度2.py
@@ -14,17 +14,20 @@ st.sidebar.title("情報とピンを立てる")
 
 # 緯度の入力方法を選択
 latitude_slider = st.sidebar.slider("緯度を選択してください", min_value=-90.000000, max_value=90.000000, value=35.689500, step=0.000001)
-latitude_input = st.sidebar.number_input("緯度を入力してください", value=latitude_slider, step=0.000001, format="%.6f")
+latitude_input = st.sidebar.number_input("緯度を入力してください", value=latitude_slider, step=0.000001, format="%.6f", key="latitude")
 
 # 経度の入力方法を選択
 longitude_slider = st.sidebar.slider("経度を選択してください", min_value=-180.000000, max_value=180.000000, value=139.691700, step=0.000001)
-longitude_input = st.sidebar.number_input("経度を入力してください", value=longitude_slider, step=0.000001, format="%.6f")
+longitude_input = st.sidebar.number_input("経度を入力してください", value=longitude_slider, step=0.000001, format="%.6f", key="longitude")
 
 # ユーザーから情報の入力を受け取る
 info = st.sidebar.text_input("情報を入力してください")
 
+# 地図の拡大率の設定を保持
+zoom_state = st.sidebar.empty()
+
 # 地図を作成
-m = folium.Map(location=[latitude_input, longitude_input], zoom_start=10)
+m = folium.Map(location=[latitude_input, longitude_input], zoom_start=zoom_state.slider("地図の拡大率を選択してください", min_value=1, max_value=20, value=10),)
 
 # 入力された緯度経度にピンを立てる
 folium.Marker([latitude_input, longitude_input], popup=info).add_to(m)
