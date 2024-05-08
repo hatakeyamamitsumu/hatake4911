69688bd5f36187972aed5bed55c53bf5135c95f3
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-05-08 21:00:36 +0900

    Update 緯度経度2.py

--- ☆Webアプリ/streamlit/pages/緯度経度2.py
+++ ☆Webアプリ/streamlit/pages/緯度経度2.py
@@ -11,7 +11,8 @@ client = gspread.authorize(creds)
 
 # タイトルを設定
 st.title("地図にピンを立てる")
-
+# 地図の拡大率の設定
+zoom_value = st.slider("地図の拡大率を固定できます", min_value=1, max_value=20, value=10)
 # 緯度の入力方法を選択
 latitude_slider = st.sidebar.slider("緯度を選択してください", min_value=-90.000000, max_value=90.000000, value=35.689500, step=0.000001)
 latitude_input = st.sidebar.number_input("緯度を入力してください", value=latitude_slider, step=0.000001, format="%.6f", key="latitude")
@@ -23,8 +24,7 @@ longitude_input = st.sidebar.number_input("経度を入力してください", v
 # ユーザーから情報の入力を受け取る
 info = st.sidebar.text_input("情報を入力してください")
 
-# 地図の拡大率の設定
-zoom_value = st.sidebar.slider("地図の拡大率を固定できます", min_value=1, max_value=20, value=10)
+
 
 # 地図を作成
 m = folium.Map(location=[latitude_input, longitude_input], zoom_start=zoom_value)
