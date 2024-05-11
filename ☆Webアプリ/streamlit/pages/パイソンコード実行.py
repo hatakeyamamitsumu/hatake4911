7c8341ec058b0900f16d3356ebc1c43371a458f1
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-05-12 07:03:18 +0900

    Update パイソンコード実行.py

--- ☆Webアプリ/streamlit/pages/パイソンコード実行.py
+++ ☆Webアプリ/streamlit/pages/パイソンコード実行.py
@@ -4,15 +4,15 @@ from googleapiclient.discovery import build
 
 # Google ドライブの認証情報
 credentials = Credentials.from_service_account_file(
-    '/path/to/your/service_account_credentials.json',  # サービスアカウントキーのファイルパスを指定してください
+    '/mount/src/hatake4911/☆Webアプリ/その他/gspread-test-421301-6cd8b0cc0e27.json',  # サービスアカウントキーのファイルパスを指定してください
     scopes=['https://www.googleapis.com/auth/drive']
 )
 
 # Google ドライブのAPIをビルド
 drive_service = build('drive', 'v3', credentials=credentials)
 
-# Googleドライブ内のPythonファイルのID
-file_id = 'your_file_id_here'
+# Googleドライブ内のPythonファイルのID https://colab.research.google.com/drive/19Rm3z4QAolOk0HoBcp7AOR9bR8YjwSTW?usp=sharing
+file_id = '19Rm3z4QAolOk0HoBcp7AOR9bR8YjwSTW'
 
 # Pythonファイルを実行して、変数aの値を取得する関数
 def execute_python_script(file_id):
