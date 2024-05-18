6ae1f229a4fb6b8b07244389c440b7a9e13c178c
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-05-18 23:21:03 +0900

    Update クローリング2.py

--- ☆Webアプリ/streamlit/pages/クローリング2.py
+++ ☆Webアプリ/streamlit/pages/クローリング2.py
@@ -25,9 +25,12 @@ def crawl_images(keyword, max_num=10):
 
 # Google Driveに画像をアップロードする関数
 def upload_to_drive(images, folder_id):
-    creds = service_account.Credentials.from_service_account_info(
-        st.secrets["gcp_service_account"]
+
     )
+    # Google ドライブ API 認証情報
+creds = Credentials.from_service_account_file(
+    '/mount/src/hatake4911/☆Webアプリ/その他/gspread-test-421301-6cd8b0cc0e27.json',
+    st.secrets['https://www.googleapis.com/auth/drive']
     drive_service = build("drive", "v3", credentials=creds)
     
     for image in images:
