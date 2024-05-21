6ac539edee191dd8b753064ca1494a4416b156b9
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-05-21 22:08:49 +0900

    Update あ.py

--- ☆Webアプリ/streamlit/pages/あ.py
+++ ☆Webアプリ/streamlit/pages/あ.py
@@ -1,2 +1,6 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+import pandas as pd
+import streamlit as st
 SP_CREDENTIAL_FILE = st.secrets["gcp"]["json_where"]
 SP_SHEET_KEY = st.secrets["gcp"]["sheet_where"]
