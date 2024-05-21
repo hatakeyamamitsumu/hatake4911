533fa3f14daed580f04d26bd918f8633ea974c38
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-05-21 20:15:45 +0900

    Update 掲示板（テキスト）.py

--- ☆Webアプリ/streamlit/pages/掲示板（テキスト）.py
+++ ☆Webアプリ/streamlit/pages/掲示板（テキスト）.py
@@ -10,7 +10,7 @@ st.title('簡易な掲示板')
 SP_CREDENTIAL_FILE = '/mount/src/hatake4911/☆Webアプリ/その他/gspread-test-421301-6cd8b0cc0e27.json'
 SP_SCOPE = ['https://www.googleapis.com/auth/drive', 'https://spreadsheets.google.com/feeds']
 SP_SHEET_KEY = '1GuaN72pbZxQJBsxLTK3n7fAQfLMJIcuUjZ7pBd-R7kc'
-
+#SP_SHEET_KEY = googledriveAPI
 # 認証情報の読み込みと認証
 credentials = ServiceAccountCredentials.from_json_keyfile_name(SP_CREDENTIAL_FILE, SP_SCOPE)
 gc = gspread.authorize(credentials)
