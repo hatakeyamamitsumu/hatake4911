82485d403fc8ca95204fcbb98d228a1d50c69a10
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-05-21 21:42:49 +0900

    Update あ.py

--- ☆Webアプリ/streamlit/pages/あ.py
+++ ☆Webアプリ/streamlit/pages/あ.py
@@ -4,7 +4,8 @@ import pandas as st
 import streamlit as st
 
 # Google Sheets 認証情報のファイルパスとスコープ
-SP_CREDENTIAL_FILE = st.secrets("aaaa")
+SP_CREDENTIAL_FILE = st.secrets["aaaa"]
+
 
 # 認証情報のパスを出力して確認する
 st.write(SP_CREDENTIAL_FILE)
