90f13e07700a2294cfaa6f7495d45d9b7683faef
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-05-21 21:24:48 +0900

    Update あ.py

--- ☆Webアプリ/streamlit/pages/あ.py
+++ ☆Webアプリ/streamlit/pages/あ.py
@@ -4,11 +4,8 @@ import pandas as pd
 import streamlit as st
 
 # Streamlitアプリケーションのタイトルを設定
-st.title('簡易な掲示板')
 
 # Google Sheets 認証情報のファイルパスとスコープ
 SP_CREDENTIAL_FILE = st.secrets["gcp"]["json_where"]
-SP_SHEET_KEY = st.secrets["gcp"]["sp_sheet_key"]
-SP_SCOPE = ['https://www.googleapis.com/auth/drive', 'https://spreadsheets.google.com/feeds']
 
 st.write(SP_CREDENTIAL_FILE)
