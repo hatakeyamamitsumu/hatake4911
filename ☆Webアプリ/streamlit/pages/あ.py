7629625e68e3cedb71fa61127cbf85fbd5edc24b
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-05-21 21:27:20 +0900

    Update あ.py

--- ☆Webアプリ/streamlit/pages/あ.py
+++ ☆Webアプリ/streamlit/pages/あ.py
@@ -1,11 +1,11 @@
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
-import pandas as pd
+import pandas as st
 import streamlit as st
-
-# Streamlitアプリケーションのタイトルを設定
 
 # Google Sheets 認証情報のファイルパスとスコープ
 SP_CREDENTIAL_FILE = st.secrets["gcp"]["json_where"]
 
+# 認証情報のパスを出力して確認する
 st.write(SP_CREDENTIAL_FILE)
+
