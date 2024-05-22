35cb3eac6f4afe17c4eaf43443bb892a7c115ce8
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-05-22 21:01:41 +0900

    Update 動画を小さく.py

--- ☆Webアプリ/streamlit/pages/動画を小さく.py
+++ ☆Webアプリ/streamlit/pages/動画を小さく.py
@@ -31,9 +31,9 @@ if uploaded_file is not None:
         # 圧縮中の進捗バー
         progress_bar = st.progress(0)
         
-        def update_progress_callback(total, current, *args):
-            progress = current / total
-            progress_bar.progress(progress)
+def update_progress_callback(progress, *args):
+    progress_bar.progress(progress)
+
 
         # 圧縮後の動画を一時ファイルに保存
         output_path = tempfile.NamedTemporaryFile(delete=False, suffix=".mp4").name
