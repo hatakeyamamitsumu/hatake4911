be3cde5a180341ed9f9f017e0a22ad1be70079ff
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-05-27 01:56:12 +0900

    Update 地図に情報を書き込む.py

--- ☆Webアプリ/streamlit/pages/地図に情報を書き込む.py
+++ ☆Webアプリ/streamlit/pages/地図に情報を書き込む.py
@@ -20,16 +20,33 @@ if app_selection == "地図にピンを立て、コメントをつけて保存�
     # 地図の拡大率の設定
     zoom_value = st.slider("地図の拡大率を固定したい時は、このスライダーをご利用ください", min_value=1, max_value=20, value=10)
     # 緯度の入力方法を選択
-    #latitude_slider = st.sidebar.slider("おおよその緯度指定", min_value=23.2100, max_value=46.3200, value=35.6895, step=0.0001)
-    #longitude_slider = st.sidebar.slider("おおよその経度指定", min_value=121.5500, max_value=146.0800, value=139.6917, step=0.0001)
-    latitude_slider = st.sidebar.slider("おおよその緯度指定", min_value=23.2100, max_value=46.3200,step=0.0001)
-    longitude_slider = st.sidebar.slider("おおよその経度指定", min_value=121.5500, max_value=146.0800,step=0.0001)
+    st.title("おおよその緯度経度検索")
+
+    # CSVファイルを読み込む
+    df = load_data(file_id)
+
+    # 都道府県名の入力欄
+    prefecture = st.text_input("都道府県名を入力してください：")
+
+    # 市区町村名の入力欄
+    city = st.text_input("市区町村名を入力してください：")
+
+    # 大字・丁目名の入力欄
+    district = st.text_input("大字・丁目名を入力してください：")
+
+    # 部分一致検索を実行
+    if prefecture or city or district:
+        filtered_df = df[df["都道府県名"].str.contains(prefecture) &
+                            df["市区町村名"].str.contains(city) &
+                            df["大字・丁目名"].str.contains(district)]
+        st.write(filtered_df)
+
     st.sidebar.write('細かく緯度経度指定')
     st.sidebar.write('＋－ボタン用の刻みを選択')
     step_size = st.sidebar.radio("0.0001=約10m, 0.001=約100m,0.01=約1000m,0.1=約10km", options=[0.0001, 0.001,0.01,0.1], index=0)
     
-    latitude_input = st.sidebar.number_input("緯度を入力してください",value=latitude_slider,step=step_size,format="%.4f",key="latitude")
-    longitude_input = st.sidebar.number_input("経度を入力してください",value=longitude_slider,step=step_size,format="%.4f",key="longitude")
+    latitude_input = st.sidebar.number_input("緯度を入力してください",step=step_size,format="%.4f",key="latitude")
+    longitude_input = st.sidebar.number_input("経度を入力してください",step=step_size,format="%.4f",key="longitude")
 
     if latitude_input != latitude_slider:
         latitude_slider = latitude_input
