94ff1c71d8fc91cf5ba5ac6537a6e7dc840b2dca
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-05-27 03:53:31 +0900

    Update 地図に情報を書き込む.py

--- ☆Webアプリ/streamlit/pages/地図に情報を書き込む.py
+++ ☆Webアプリ/streamlit/pages/地図に情報を書き込む.py
@@ -21,8 +21,8 @@ if app_selection == "地図にピンを立て、コメントをつけて保存�
     zoom_value = st.slider("地図の拡大率を固定したい時は、このスライダーをご利用ください", min_value=7, max_value=20, value=10)
     # 緯度の入力方法を選択
 
-    latitude_slider = st.sidebar.slider("おおよその緯度指定", min_value=23.2100, max_value=46.3200, value=35.6895, step=0.0001)
-    longitude_slider = st.sidebar.slider("おおよその経度指定", min_value=121.5500, max_value=146.0800, value=139.6917, step=0.0001)
+    latitude_slider = st.slider("おおよその緯度指定", min_value=23.2100, max_value=46.3200, value=35.6895, step=0.0001)
+    longitude_slider = st.sslider("おおよその経度指定", min_value=121.5500, max_value=146.0800, value=139.6917, step=0.0001)
 
     st.sidebar.write('細かく緯度経度指定')
     st.sidebar.write('＋－ボタン用の刻みを選択')
