c337287a08992bc7b5c369e4803486bfb1695335
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-05-27 01:58:24 +0900

    Update 地図に情報を書き込む.py

--- ☆Webアプリ/streamlit/pages/地図に情報を書き込む.py
+++ ☆Webアプリ/streamlit/pages/地図に情報を書き込む.py
@@ -28,11 +28,7 @@ if app_selection == "地図にピンを立て、コメントをつけて保存�
     latitude_input = st.sidebar.number_input("緯度を入力してください",step=step_size,format="%.4f",key="latitude")
     longitude_input = st.sidebar.number_input("経度を入力してください",step=step_size,format="%.4f",key="longitude")
 
-    if latitude_input != latitude_slider:
-        latitude_slider = latitude_input
-    
-    if longitude_input != longitude_slider:
-        longitude_slider = longitude_input
+
 
 
 
