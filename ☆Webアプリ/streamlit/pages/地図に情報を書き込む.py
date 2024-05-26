996280da904127d1a3a816d4023fdfc3a33535d8
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-05-26 23:45:47 +0900

    Update 地図に情報を書き込む.py

--- ☆Webアプリ/streamlit/pages/地図に情報を書き込む.py
+++ ☆Webアプリ/streamlit/pages/地図に情報を書き込む.py
@@ -30,7 +30,7 @@ if app_selection == "地図にピンを立て、コメントをつけて保存�
     
     # 経度の入力方法を選択
     longitude_slider = st.sidebar.slider("経度を選択してください", min_value=121.5500, max_value=146.0800, value=139.6917, step=0.0001)
-        step_size = st.sidebar.radio("ステップサイズを選んでください", options=[0.0001, 0.001,0.01], index=0)
+    step_size = st.sidebar.radio("ステップサイズを選んでください", options=[0.0001, 0.001,0.01], index=0)
     longitude_input = st.sidebar.number_input("緯度を入力してください",value=longitude_slider,step=step_size,format="%.4f",key="longitude")
     #longitude_input = st.sidebar.number_input("経度を入力してください", value=longitude_slider, step=0.0001, format="%.4f", key="longitude")
 
