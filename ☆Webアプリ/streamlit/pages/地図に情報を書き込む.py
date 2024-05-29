eb8c3cad8059bc78dd3ce80bebd1b687c56de0ed
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-05-29 11:11:48 +0900

    Update 地図に情報を書き込む.py

--- ☆Webアプリ/streamlit/pages/地図に情報を書き込む.py
+++ ☆Webアプリ/streamlit/pages/地図に情報を書き込む.py
@@ -36,6 +36,31 @@ if app_selection == "地図のおすすめスポットにピンを立てる":
     m = folium.Map(location=[latitude_input, longitude_input], zoom_start=10)
     folium.Marker([latitude_input, longitude_input], popup=folium.Popup(info, max_width=300)).add_to(m)
 
+
+
+            st.title("おおよその緯度経度検索")
+
+        # CSVファイルを読み込む
+        df = load_data(file_id)
+        # 都道府県名の入力欄
+        prefecture = st.text_input("都道府県名を入力してください：")
+        
+        # 市区町村名の入力欄
+        city = st.text_input("市区町村名を入力してください：")
+        
+        # 大字・丁目名の入力欄
+        district = st.text_input("大字・丁目名を入力してください：")
+        
+        # 部分一致検索を実行
+        if prefecture or city or district:
+            filtered_df = df[df["都道府県名"].str.contains(prefecture) &
+                            df["市区町村名"].str.contains(city) &
+                            df["大字・丁目名"].str.contains(district)]
+        st.write(filtered_df)
+
+
+
+    
     # MousePositionプラグインを追加して現在の座標を表示
     MousePosition(position='topleft', separator=' | ', prefix="現在の座標：").add_to(m)
     
@@ -67,25 +92,7 @@ if app_selection == "地図のおすすめスポットにピンを立てる":
 
 
         
-        st.title("おおよその緯度経度検索")
 
-        # CSVファイルを読み込む
-        df = load_data(file_id)
-        # 都道府県名の入力欄
-        prefecture = st.text_input("都道府県名を入力してください：")
-        
-        # 市区町村名の入力欄
-        city = st.text_input("市区町村名を入力してください：")
-        
-        # 大字・丁目名の入力欄
-        district = st.text_input("大字・丁目名を入力してください：")
-        
-        # 部分一致検索を実行
-        if prefecture or city or district:
-            filtered_df = df[df["都道府県名"].str.contains(prefecture) &
-                            df["市区町村名"].str.contains(city) &
-                            df["大字・丁目名"].str.contains(district)]
-        st.write(filtered_df)
     
 
 
