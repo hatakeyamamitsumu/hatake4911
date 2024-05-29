6dc34b4298fbbd1aecb4ca2dc704967b8bdc465c
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-05-29 11:02:56 +0900

    Update 地図に情報を書き込む.py

--- ☆Webアプリ/streamlit/pages/地図に情報を書き込む.py
+++ ☆Webアプリ/streamlit/pages/地図に情報を書き込む.py
@@ -66,32 +66,7 @@ if app_selection == "地図のおすすめスポットにピンを立てる":
 
 
     
-     # Streamlitアプリのセットアップ
-    def main():
-        st.title("おおよその緯度経度検索")
 
-        # CSVファイルを読み込む
-        df = load_data(file_id)
-
-        # 都道府県名の入力欄
-        prefecture = st.text_input("都道府県名を入力してください：")
-
-        # 市区町村名の入力欄
-        city = st.text_input("市区町村名を入力してください：")
-
-        # 大字・丁目名の入力欄
-        district = st.text_input("大字・丁目名を入力してください：")
-
-        # 部分一致検索を実行
-        if prefecture or city or district:
-            filtered_df = df[df["都道府県名"].str.contains(prefecture) &
-                             df["市区町村名"].str.contains(city) &
-                             df["大字・丁目名"].str.contains(district)]
-            st.write(filtered_df)
-
-    # Streamlitアプリを実行
-    if __name__ == "__main__":
-        main()
 
 
 
@@ -125,3 +100,26 @@ elif app_selection == "地図上のすべてのピンを表示":
 
     # 地図を表示
     folium_static(m)
+
+
+
+
+st.title("おおよその緯度経度検索")
+
+    # CSVファイルを読み込む
+    df = load_data(file_id)
+# 都道府県名の入力欄
+prefecture = st.text_input("都道府県名を入力してください：")
+
+# 市区町村名の入力欄
+city = st.text_input("市区町村名を入力してください：")
+
+# 大字・丁目名の入力欄
+district = st.text_input("大字・丁目名を入力してください：")
+
+# 部分一致検索を実行
+if prefecture or city or district:
+filtered_df = df[df["都道府県名"].str.contains(prefecture) &
+                df["市区町村名"].str.contains(city) &
+                df["大字・丁目名"].str.contains(district)]
+st.write(filtered_df)
