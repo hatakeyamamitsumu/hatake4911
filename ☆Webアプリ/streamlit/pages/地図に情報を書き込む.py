4da74eb8f13f5d7d450c46868c0e4508c93c3cd8
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-05-29 11:33:19 +0900

    Update 地図に情報を書き込む.py

--- ☆Webアプリ/streamlit/pages/地図に情報を書き込む.py
+++ ☆Webアプリ/streamlit/pages/地図に情報を書き込む.py
@@ -64,28 +64,7 @@ if app_selection == "地図のおすすめスポットにピンを立てる":
         # ユーザーに成功メッセージを表示
         st.sidebar.success("情報と緯度経度がGoogle Sheetsに書き込まれました。")
 
-    def search_coordinates():
-        st.title("おおよその緯度経度検索")
-    
-        # CSVファイルを読み込む
-        file_id = "1fDInJTb7My6by9Dx70XIByDh8yux-09i"
-        df = load_data(file_id)
-        # 都道府県名の入力欄
-        prefecture = st.text_input("都道府県名を入力してください：")
-            
-        # 市区町村名の入力欄
-        city = st.text_input("市区町村名を入力してください：")
-            
-        # 大字・丁目名の入力欄
-        district = st.text_input("大字・丁目名を入力してください：")
-            
-        # 部分一致検索を実行
-        if prefecture or city or district:
-            filtered_df = df[df["都道府県名"].str.contains(prefecture) &
-                            df["市区町村名"].str.contains(city) &
-                            df["大字・丁目名"].str.contains(district)]
-            st.write(filtered_df)
-    search_coordinates()
+
 
 
 
