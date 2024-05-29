162f7bb28709ea51b2d9f76661516d1a93ee29cf
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-05-29 11:10:09 +0900

    Update 地図に情報を書き込む.py

--- ☆Webアプリ/streamlit/pages/地図に情報を書き込む.py
+++ ☆Webアプリ/streamlit/pages/地図に情報を書き込む.py
@@ -65,6 +65,27 @@ if app_selection == "地図のおすすめスポットにピンを立てる":
         st.sidebar.success("情報と緯度経度がGoogle Sheetsに書き込まれました。")
 
 
+
+        
+        st.title("おおよその緯度経度検索")
+
+        # CSVファイルを読み込む
+        df = load_data(file_id)
+        # 都道府県名の入力欄
+        prefecture = st.text_input("都道府県名を入力してください：")
+        
+        # 市区町村名の入力欄
+        city = st.text_input("市区町村名を入力してください：")
+        
+        # 大字・丁目名の入力欄
+        district = st.text_input("大字・丁目名を入力してください：")
+        
+        # 部分一致検索を実行
+        if prefecture or city or district:
+            filtered_df = df[df["都道府県名"].str.contains(prefecture) &
+                            df["市区町村名"].str.contains(city) &
+                            df["大字・丁目名"].str.contains(district)]
+        st.write(filtered_df)
     
 
 
@@ -104,22 +125,4 @@ elif app_selection == "地図上のすべてのピンを表示":
 
 
 
-st.title("おおよその緯度経度検索")
 
-    # CSVファイルを読み込む
-df = load_data(file_id)
-# 都道府県名の入力欄
-prefecture = st.text_input("都道府県名を入力してください：")
-
-# 市区町村名の入力欄
-city = st.text_input("市区町村名を入力してください：")
-
-# 大字・丁目名の入力欄
-district = st.text_input("大字・丁目名を入力してください：")
-
-# 部分一致検索を実行
-if prefecture or city or district:
-    filtered_df = df[df["都道府県名"].str.contains(prefecture) &
-                    df["市区町村名"].str.contains(city) &
-                    df["大字・丁目名"].str.contains(district)]
-st.write(filtered_df)
