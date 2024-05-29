39a29331884e8046ea355dba2dc4eea07c3699ce
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-05-29 11:57:34 +0900

    Update 地図に情報を書き込む.py

--- ☆Webアプリ/streamlit/pages/地図に情報を書き込む.py
+++ ☆Webアプリ/streamlit/pages/地図に情報を書き込む.py
@@ -11,7 +11,6 @@ scope = ['https://www.googleapis.com/auth/drive', 'https://spreadsheets.google.c
 creds = ServiceAccountCredentials.from_json_keyfile_dict(st.secrets["google"], scope)
 client = gspread.authorize(creds)
 file_id = "1fDInJTb7My6by9Dx70XIByDh8yux-09i"
-file_id2="X1mppebuIXGIGd-n_9pL6wHahk1-rFbO2tAjgc9mEqg"
 # セッション状態にクリックされた位置の緯度と経度を保存
 if "latitude" not in st.session_state:
     st.session_state.latitude = 35.0000
@@ -96,38 +95,5 @@ elif app_selection == "地図上のすべてのピンを表示":
     #folium_static(m)
     # folium_static(m)
     st_folium(m)
-
-
-# CSVファイルを読み込む関数
-def load_data(file_id2):
-    url = f"https://docs.google.com/spreadsheets/d/{file_id2}/export?format=csv"
-    return pd.read_csv(url)
-
-# Streamlitアプリのセットアップ
-st.title("おおよその緯度経度検索")
-
-
-
-
-# CSVファイルを読み込む
-df = load_data(file_id2)
-
-# 都道府県名の入力欄
-prefecture = st.text_input("都道府県名を入力してください：")
-
-# 市区町村名の入力欄
-city = st.text_input("市区町村名を入力してください：")
-
-# 大字・丁目名の入力欄
-district = st.text_input("大字・丁目名を入力してください：")
-
-# 部分一致検索を実行
-if prefecture or city or district:
-    filtered_df = df[df["都道府県名"].str.contains(prefecture) &
-                     df["市区町村名"].str.contains(city) &
-                     df["大字・丁目名"].str.contains(district)]
-    st.write(filtered_df)
-
-
 
 
