39fe122a2f3810b8e01325fa4dd1b32f23f2b2a7
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-06-07 07:32:52 +0900

    Update 地図に情報を書き込む.py

--- ☆Webアプリ/streamlit/pages/地図に情報を書き込む.py
+++ ☆Webアプリ/streamlit/pages/地図に情報を書き込む.py
@@ -152,7 +152,7 @@ elif app_selection == "地図上のすべてのピンを表示":
     data = sheet.get_all_values()
 
     # 地図を作成
-    m = folium.Map(width=900, height=300,location=[35.0000, 135.0000], zoom_start=9)
+    m = folium.Map(width=900, height=300,location=[35.0000, 135.0000], zoom_start=9,zoom_control=False)
 
     # データから緯度経度を取得し、ピンを立てる
     for row in data[1:]:  # ヘッダーを除く
