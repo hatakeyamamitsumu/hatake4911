46fca2d14f6cb22cf0e28cae37acb8aef52cbdfd
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-06-10 06:47:09 +0900

    Create 0から9.py

---
+++ ☆Webアプリ/streamlit/pages/0から9.py
@@ -0,0 +1,25 @@
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.utils import to_categorical
+
+# データセットのロード
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+# ラベルをone-hotエンコーディングに変換
+y_train = to_categorical(y_train)
+y_test = to_categorical(y_test)
+
+# モデルの構築
+model = Sequential([
+    Flatten(input_shape=(28, 28)),
+    Dense(128, activation='relu'),
+    Dense(10, activation='softmax')
+])
+
+# モデルのコンパイル
+model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+
+# モデルのトレーニング
+model.fit(x_train, y_train, epochs=5, validation_data=(x_test, y_test))
