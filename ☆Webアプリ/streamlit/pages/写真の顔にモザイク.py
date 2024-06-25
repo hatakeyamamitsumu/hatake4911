a80314cafb75669265bb4769b08db4defcfd4a9d
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-06-26 00:39:10 +0900

    Update 写真の顔にモザイク.py

--- ☆Webアプリ/streamlit/pages/写真の顔にモザイク.py
+++ ☆Webアプリ/streamlit/pages/写真の顔にモザイク.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import numpy as np
-from PIL import Image
+from PIL import Image, ImageDraw
 import cv2
 from mtcnn import MTCNN
 
@@ -10,11 +10,15 @@ detector = MTCNN()
 def detect_faces(image):
     # 顔を検出
     faces = detector.detect_faces(image)
-    # 顔の位置を取得
-    face_positions = [(face['box'][0], face['box'][1], face['box'][2], face['box'][3]) for face in faces]
-    return face_positions
+    # 検出された顔に矩形を描画して、顔の位置を返す
+    face_positions = []
+    for face in faces:
+        x, y, width, height = face['box']
+        cv2.rectangle(image, (x, y), (x + width-1, y + height-1), (255, 0, 0), 0)
+        face_positions.append((x, y, width, height))
+    return image, face_positions
 
-def apply_mosaic(image, face_positions, scale=0.5):
+def apply_mosaic(image, face_positions, scale=0.05):
     for (x, y, width, height) in face_positions:
         # 顔の部分を切り取る
         face = image[y:y+height, x:x+width]
@@ -25,7 +29,7 @@ def apply_mosaic(image, face_positions, scale=0.5):
         image[y:y+height, x:x+width] = face
     return image
 
-st.title("顔認識アプリ")
+st.title("顔認識とモザイク処理アプリ")
 st.write("jpg画像をアップロードしてください。")
 
 # 画像のアップロード
@@ -35,11 +39,10 @@ if uploaded_file is not None:
     # アップロードされた画像を読み込む
     image = Image.open(uploaded_file)
     # OpenCV形式に変換
-    image = np.array(image)
-    # 顔認識を実行
-    face_positions = detect_faces(image)
-    # モザイク処理を適用
-    st.write("写真上で右クリック　→　名前を付けて画像を保存　で写真を取得できます。")
-    result_image = apply_mosaic(image, face_positions)
-    # 結果の画像を表示
-    st.image(result_image, caption="認識結果", use_column_width=True)
+    image_cv = np.array(image)
+    # 顔認識を実行し、顔の位置を取得
+    result_image, face_positions = detect_faces(image_cv.copy())
+    # 顔にモザイク処理を適用
+    result_image_mosaic = apply_mosaic(result_image, face_positions)
+    # モザイクをかけた結果の画像を表示
+    st.image(result_image_mosaic, caption="モザイク処理後の画像", use_column_width=True)
