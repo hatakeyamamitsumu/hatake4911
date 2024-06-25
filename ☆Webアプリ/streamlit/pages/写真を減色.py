493668f1e3bc7a4c0175f2f059815834de1649cc
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-06-25 22:33:41 +0900

    Update 写真を減色.py

--- ☆Webアプリ/streamlit/pages/写真を減色.py
+++ ☆Webアプリ/streamlit/pages/写真を減色.py
@@ -14,17 +14,21 @@ if uploaded_file is not None:
 
     # 画像の減色処理
     with st.spinner('処理中...'):
-        quantized_image = ImageOps.posterize(original_image, num_colors)
+        # 減色処理のために色の深さに応じてビット数を計算
+        bits = 8 - num_colors.bit_length()
+
+        # 画像の減色処理を実行
+        quantized_image = ImageOps.posterize(original_image, bits)
 
     st.success("処理が完了しました！")
 
     # 減色後の画像を表示
-    st.image(quantized_image, caption=f"Quantized Image (Colors: {num_colors})", use_column_width=True)
+    st.image(quantized_image, caption=f"Quantized Image (Colors: {2 ** bits})", use_column_width=True)
 
     # ダウンロードリンクを提供
     if st.button('Download Quantized Image'):
         # ファイル名を設定
-        download_filename = f"quantized_image_{num_colors}_colors.png"
+        download_filename = f"quantized_image_{2 ** bits}_colors.png"
         # ファイルをダウンロード
         quantized_image.save(download_filename)
         st.download_button(label='Click here to download',
