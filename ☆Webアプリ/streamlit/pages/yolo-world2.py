7457e85e436833b7d35574ad43f4460e9c8294fd
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-10-06 02:35:42 +0900

    Update yolo-world2.py

--- ☆Webアプリ/streamlit/pages/yolo-world2.py
+++ ☆Webアプリ/streamlit/pages/yolo-world2.py
@@ -4,7 +4,7 @@ import cv2
 import numpy as np
 
 # モデルの読み込み (パスを適宜変更)
-model = YOLOW8('yolov8s.pt')
+model = YOLOWorld('/mount/src/hatake4911/☆Webアプリ/その他重要ファイル/yolov8s.pt')
 
 def detect_objects(uploaded_image, model):
     """
