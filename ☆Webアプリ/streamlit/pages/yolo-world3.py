58df40849880bfddaae3bd0ee60db55f890823b7
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-10-07 00:53:48 +0900

    Update yolo-world3.py

--- ☆Webアプリ/streamlit/pages/yolo-world3.py
+++ ☆Webアプリ/streamlit/pages/yolo-world3.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 import cv2
 import numpy as np
 
-# 　モデルの読み込み
+# モデルの読み込み
 model = YOLOWorld('/mount/src/hatake4911/☆Webアプリ/その他重要ファイル/yolov8s.pt') # モデルのパスを適宜変更
 
 # カメラキャプチャ
