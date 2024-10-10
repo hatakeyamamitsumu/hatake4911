4cbdc63c292a9c11636d0b3fac32d46b3d68c429
hatakeyamamitsumu <143398408+hatakeyamamitsumu@users.noreply.github.com>
2024-10-11 08:38:31 +0900

    Update yoloで姿勢推定.py

--- ☆Webアプリ/streamlit/pages/yoloで姿勢推定.py
+++ ☆Webアプリ/streamlit/pages/yoloで姿勢推定.py
@@ -1,14 +1,10 @@
 
-
-import cv2
 import streamlit as st
 from ultralytics import YOLO
-import numpy as np
-
 
 # モデルのロード
-model = YOLO("/mount/src/hatake4911/☆Webアプリ/その他重要ファイル/yolo11n-pose.pt")  # 姿勢推定モデル
-
+#model = YOLO("yolov8n-pose.pt")  # 姿勢推定モデル
+model = YOLO("/mount/src/hatake4911/☆Webアプリ/その他重要ファイル/yolov8n-pose.pt")  # 姿勢推定モデル
 # タイトル設定
 st.title("YOLOによる姿勢推定アプリ")
 
@@ -16,25 +12,10 @@ st.title("YOLOによる姿勢推定アプリ")
 uploaded_file = st.file_uploader("画像を選択してください", type=["png", "jpg", "jpeg"])
 
 if uploaded_file is not None:
-    try:
-        # 画像の読み込み
-        file_bytes = np.frombuffer(uploaded_file.read(), np.uint8)
-        img = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
-
-        # 推論実行
-        results = model(img)
-
-        # 推論結果の表示
-        # results.print()  # 推論結果の詳細を表示
-        # results.show()  # 画像上に結果を可視化
-
-        # 推論結果から画像を取得し、Streamlitで表示
-        if len(results.xyxy) > 0:
-            # 検出結果がある場合
-            img = results.plot()[0]  # 最初の検出結果の画像を取得
-            st.image(img)
-        else:
-            # 検出結果がない場合
-            st.write("人物が検出されませんでした")
-    except Exception as e:
-        st.error(f"エラーが発生しました: {e}")
+    # 画像の読み込み
+    img = uploaded_file.read()
+    # 推論実行
+    results = model(img)
+    # 結果の表示
+    for img in results.imgs:
+        st.image(img)
